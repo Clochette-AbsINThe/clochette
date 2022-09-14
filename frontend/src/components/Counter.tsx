cf85e0528df7b00cb93f298af6318bedbaa8dfcc
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-14 15:17:53 +0200

    Change style for Counter

--- frontend/src/components/Counter.tsx
+++ frontend/src/components/Counter.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import './Counter.scss'
+import React, { useState } from 'react';
 
 interface CounterProps {
     children: JSX.Element
@@ -7,18 +6,18 @@ interface CounterProps {
 }
 
 export default function Counter({ children, count: initialCount }: CounterProps): JSX.Element {
-    const [count, setCount] = useState(initialCount)
-    const add = (): void => setCount((i) => i + 1)
-    const subtract = (): void => setCount((i) => i - 1)
+    const [count, setCount] = useState(initialCount);
+    const add = (): void => setCount((i) => i + 1);
+    const subtract = (): void => setCount((i) => i - 1);
 
     return (
         <>
-            <div className="counter">
+            <div className="grid text-3xl grid-cols-3 mt-8 place-items-center">
                 <button onClick={subtract}>-</button>
                 <pre>{count}</pre>
                 <button onClick={add}>+</button>
             </div>
-            <div className="counter-message">{children}</div>
+            <div className="text-center">{children}</div>
         </>
-    )
+    );
 }
