813e50c975a727ad742afba21e5e4cc21494bb04
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-14 15:16:34 +0200

    Create Footer

---
+++ frontend/src/layouts/include/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer(): JSX.Element {
+    return (
+        <footer className="flex">
+            <p className="justify-center self-center ml-6 mr-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore a possimus voluptatem quo aliquam ducimus quibusdam quaerat suscipit, distinctio vel, cum dignissimos deleniti fugit aliquid nesciunt qui illo voluptas sint.</p>
+        </footer>
+    );
+}
