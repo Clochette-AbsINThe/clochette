fc0624245e223140f915b1cbe951cf55e28903d4
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-14 15:13:51 +0200

    Add AstroCompress

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
     // Enable React to support React JSX components.
-    integrations: [react(), tailwind()]
-})
+    integrations: [react(), tailwind(), compress()]
+});
