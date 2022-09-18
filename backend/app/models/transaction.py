6321206be3e22173a5ce6b5bc5567305cd7be04d
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-09-19 01:07:00 +0200

    Complete transaction sqlalchemy model

---
+++ backend/app/models/transaction.py
@@ -0,0 +1,22 @@
+import enum
+
+from sqlalchemy import Boolean, Column, DateTime, Enum, Integer, Float, ForeignKey
+
+from app.db.base_class import Base
+
+
+class PaymentMethod(str, enum.Enum):
+    LYDIA = "Lydia"
+    PUMPKIN = "Pumpkin"
+    SEPA = "Virement SEPA"
+    CASH = "Espèces"
+    TPE = "Carte bancaire"
+
+
+class Transaction(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    treasury_id = Column(Integer, ForeignKey("treasury.id"))
+    datetime = Column(DateTime, nullable=False)
+    payment_method = Column(Enum(PaymentMethod), nullable=False)
+    amount = Column(Float, nullable=False)
+    sale = Column(Boolean, nullable=False) # True means selling, False means buying
