d0f5f58c0954bd36872e647b15b93dd01d70df1e
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-21 02:02:51 +0200

    Calculate the total price of drinks

--- frontend/src/components/Glass.tsx
+++ frontend/src/components/Glass.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ItemCount, ItemCountProps } from '@components/ItemCount';
 
 const items: ItemCountProps[] = [
@@ -27,11 +27,25 @@ const verreItem: ItemCountProps = {
 };
 
 export default function Glass(): JSX.Element {
-    const [verre, setVerre] = useState(items.map(() => 0));
+    const [boissons, setBoissons] = useState(items.map(() => 0));
+    const [nbVerres, setNbVerres] = useState(0);
+    const [total, setTotal] = useState(0);
 
     function onValueChange(value: number, key: number): void {
-        setVerre(verre.map((v, i) => (i === key ? value : v)));
+        setBoissons(boissons.map((v, i) => (i === key ? value : v)));
     }
+
+    function onVerreChange(value: number): void {
+        setNbVerres(value);
+    }
+
+    useEffect(() => {
+        setTotal(boissons.reduce((acc, v, i) => acc + v * items[i].price, 0) + (boissons.reduce((a, b) => a + b, 0) + nbVerres) * verreItem.price);
+    }, [boissons, nbVerres]);
+
+    useEffect(() => {
+        console.log('Total: ', total);
+    }, [total]);
 
     return (
         <div className="h-full flex flex-col border-2 rounded border-gray-800 dark:border-gray-300">
@@ -39,7 +53,7 @@ export default function Glass(): JSX.Element {
                 <ItemCount key={i} id={i} name={item.name} price={item.price} onValueChange={onValueChange} />
             ))}
             <div className='flex flex-col grow justify-end mt-[5rem]'>
-                <ItemCount name={verreItem.name} price={verreItem.price} value={verre.reduce((a, b) => a + b, 0)} image={verreItem.image} />
+                <ItemCount name={verreItem.name} price={verreItem.price} value={boissons.reduce((a, b) => a + b, 0)} image={verreItem.image} onValueChange={onVerreChange} />
             </div>
         </div>
     );
