c9ec3000fdb03762c0bb7160fc0496ebe3337c1e
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-09-21 22:32:13 +0200

    Create FastAPI entryfile

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, APIRouter
+
+
+app = FastAPI(
+    title="Clochette API",
+    openapi_url="/openapi.json",
+)
+
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    return {
+        "msg": "Hello, World!",
+    }
+
+
+app.include_router(api_router)
