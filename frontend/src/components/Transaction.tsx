bb4f2fe7737d57ceefaaa4749225d3bca584c154
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-26 23:26:02 +0200

    Create the logic with API calls #16 #8

--- frontend/src/components/Transaction.tsx
+++ frontend/src/components/Transaction.tsx
@@ -1,91 +1,123 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import TransactionSwitch, { TransactionEnum } from '@components/TransactionSwitch';
-import Glass from '@components/Glass';
-import type { ItemCountProps } from '@components/ItemCount';
-import Popup from 'reactjs-popup';
+import Sale from '@components/Sale';
 import PopupWindows from '@components/PopupWindows';
+import { getBoissons } from '@proxies/getBoissons';
+import { getConsommables } from '@proxies/getConsommables';
+import { getHorsStocks } from '@proxies/getHorsStocks';
 
-const items: ItemCountProps[] = [
-    {
-        name: 'Bière 1',
-        price: 3.5
-    },
-    {
-        name: 'Bière 2',
-        price: 4
-    },
-    {
-        name: 'Cidre',
-        price: 3
-    },
-    {
-        name: 'Bière 3',
-        price: 3.4
-    }
-];
-
-const verreItem: ItemCountProps = {
-    name: 'Verre',
-    price: 1,
-    image: '/EcoCup.png'
-};
+export interface ItemTypes {
+    id: number
+    name: string
+    price: number
+    value: number
+    image?: string
+    isGlass?: boolean
+}
 
 export default function Transaction(): JSX.Element {
-    const [totalPrice, setTotalPrice] = React.useState(0);
-
-    function changeTotalPrice(price: number): void {
-        setTotalPrice(price);
-    };
-
     const [transactionType, setTransactionType] = React.useState(TransactionEnum.Vente);
 
-    function changeTransactionType(type: TransactionEnum): void {
-        setTransactionType(type);
-    };
+    const [itemsBoissons, setItemsBoissons] = useState<ItemTypes[]>([]);
+    const { getData: getDataGlass, loading: loadingGlass } = getBoissons(setItemsBoissons);
+
+    const [itemsConsommable, setItemsConsommable] = useState<ItemTypes[]>([]);
+    const { getData: getDataConsommables, loading: loadingConsommable } = getConsommables(setItemsConsommable);
+
+    const [itemsHorsStock, setItemsHorsStock] = useState<ItemTypes[]>([]);
+    const { getData: getDataHorsStock, loading: loadingHorsStock } = getHorsStocks(setItemsHorsStock);
+
+    const allItems = [...itemsBoissons, ...itemsConsommable, ...itemsHorsStock];
+
+    const [totalPrice, setTotalPrice] = React.useState(0);
+
+    useEffect(() => {
+        setTotalPrice(allItems.reduce((acc, item) => acc + (item.value * item.price), 0));
+    }, [allItems]);
 
     useEffect(() => {
         setTotalPrice(0);
+        if (transactionType === TransactionEnum.Vente) {
+            getDataGlass();
+            getDataConsommables();
+            getDataHorsStock();
+        }
     }, [transactionType]);
 
-
+    function handleChangeTransactionType(type: TransactionEnum): void {
+        setTransactionType(type);
+    };
 
     function renderTransaction(): JSX.Element {
         if (transactionType === TransactionEnum.Vente) {
             return (
-                <div className="grid-cols-3 flex-grow grid gap-x-2">
-                    <Glass items={items} verreItem={verreItem} changeSubTotalPrice={changeTotalPrice} />
-                    {/* <Glass items={items} verreItem={verreItem} />
-                    <Glass items={items} verreItem={verreItem} /> */}
+                <div className="md:grid-cols-3 flex-grow grid gap-2 grid-cols-1">
+                    <Sale
+                        items={itemsBoissons}
+                        changeSubTotal={(setItemsBoissons)}
+                        loading={loadingGlass}
+                    />
+                    <Sale
+                        items={itemsConsommable}
+                        changeSubTotal={setItemsConsommable}
+                        loading={loadingConsommable}
+                    />
+                    <Sale
+                        items={itemsHorsStock}
+                        changeSubTotal={setItemsHorsStock}
+                        loading={loadingHorsStock}
+                    />
                 </div>
             );
         } else {
             return (
                 <div className="flex-grow">ACHAT</div>
-
             );
         }
     };
 
+    function renderRecap(): JSX.Element {
+        return (
+            <div className='flex flex-col mx-4 my-3 flex-grow'>
+                {
+                    allItems.sort((a, b) => b.price * b.value - a.price * a.value).map((item) => {
+                        if (item.value > 0) {
+                            return (
+                                <li className='text-2xl flex justify-between mb-2 border-b border-b-orange-300' key={item.name}>
+                                    <div className='flex'>
+                                        <div className='mr-2'>{item.value}</div>
+                                        <div>{item.name}</div>
+                                    </div>
+                                    <div>{item.value * item.price}€</div>
+                                </li>
+                            );
+                        } else {
+                            return null;
+                        }
+                    })
+                }
+            </div>
+        );
+    }
+
     return (
         <>
-            <TransactionSwitch changeTransactionType={changeTransactionType} />
+            <TransactionSwitch changeTransactionType={handleChangeTransactionType} />
             {renderTransaction()}
             <div className='flex justify-end mt-3'>
                 <div className='text-2xl font-bold mr-8'>Total: {totalPrice}€</div>
-                <Popup trigger={<button className='bg-green-700 text-white font-bold py-2 px-4 rounded'>Valider</button>} modal>
-                    {(close) => {
-                        return (
-                            <PopupWindows close={close}>
-                                <div className='flex flex-col'>
-                                    <div className='text-2xl font-bold'>Total: {totalPrice}€</div>
-                                </div>
-                            </PopupWindows>
-                        );
-                    }
-                    }
-                </Popup>
-            </div>
 
+                <PopupWindows trigger={{ className: 'bg-green-700 text-white font-bold py-2 px-4 rounded', content: 'Valider' }}>
+                    <div className='flex flex-col flex-grow'>
+                        <div className='text-3xl font-bold pb-2 border-b-2 border-neutral-400'>Récapitulatif de la commande :</div>
+                        {renderRecap()}
+                        <div className='flex pt-3 self-end'>
+                            <div className='text-2xl font-bold mr-8'>Total: {totalPrice}€</div>
+                            <button className='bg-green-700 text-white font-bold py-2 px-4 rounded'>Valider</button>
+                        </div>
+                    </div>
+                </PopupWindows>
+            </div>
         </>
     );
 };
