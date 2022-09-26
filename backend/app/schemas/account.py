6673f9f18b009d73bb19bed8404ced359782d0c5
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-09-27 01:58:51 +0200

    Add transactions attribute to validation schema

--- backend/app/schemas/account.py
+++ backend/app/schemas/account.py
@@ -1,1 +1,30 @@
 from pydantic import BaseModel
+
+
+class AccountBase(BaseModel):
+    username: str
+    password: str
+    roles: str
+    is_active: bool
+    last_name: str
+    first_name: str
+    amount: float
+    promotion_year: int
+    staff_name: str
+    is_inducted: bool
+
+
+class AccountCreate(AccountBase):
+    pass
+
+
+class AccountUpdate(AccountBase):
+    id: int
+
+
+class Account(AccountBase):
+    id: int
+    transactions: list = []
+
+    class Config:
+        orm_mode = True
