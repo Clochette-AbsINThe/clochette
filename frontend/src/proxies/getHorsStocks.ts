ac6f1e0a7c032f75bff7d7910670550dc9b4b439
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-26 23:22:56 +0200

    Create getHorsStocks proxy #15

---
+++ frontend/src/proxies/getHorsStocks.ts
@@ -0,0 +1,31 @@
+import type { ItemTypes } from '@components/Transaction';
+import useAxios from 'axios-hooks';
+
+interface GetHorsStocksType {
+    getData: () => void
+    loading: boolean
+}
+
+export function getHorsStocks(setItem: (value: ItemTypes[]) => void): GetHorsStocksType {
+    const [{ error, loading }, get] = useAxios<ItemTypes[]>('https://clochette.dev/api/hors-stocks', { manual: true });
+
+    const getDataAsync = async (): Promise<void> => {
+        const { data, status } = (await get());
+        if (status === 200) {
+            data.forEach((item) => {
+                item.value = 0;
+            });
+            setItem(data);
+        } else {
+            console.error(error);
+        }
+    };
+
+    const getData = (): void => {
+        void getDataAsync();
+    };
+    return {
+        getData,
+        loading
+    };
+}
