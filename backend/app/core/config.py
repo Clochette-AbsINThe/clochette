c3a8bbcca24efd2868ec36ffa3c61633d0ce35c8
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-09-27 01:53:37 +0200

    Add configuration package

---
+++ backend/app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, validator
+
+
+class Settings(BaseSettings):
+    API_V1_PREFIX: str = "/api/v1"
+
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    BACKEND_CORS_ORIGIN: list[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGIN", pre=True)
+    def assemble_cors_origins(cls, v: str | list[str]) -> str | list[str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    SQLALCHEMY_DATABASE_URI: str | None = ""
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
