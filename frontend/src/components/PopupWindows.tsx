300da89ae8e060256ff65d2e9aad29bcb4a4d801
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-09-30 14:18:34 +0200

    Popup test #18

--- frontend/src/components/PopupWindows.tsx
+++ frontend/src/components/PopupWindows.tsx
@@ -29,7 +29,7 @@ export default function PopupWindows(props: PopupWindowsProps): JSX.Element {
         if (isOpen) {
             return (
                 <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#88888888] px-3">
-                    <div ref={ref} className="bg-gray-100 dark:bg-slate-900 rounded-lg p-4 flex flex-col border-2 dark:border-white border-black min-w-[75vw] min-h-[75vh] opacity-100" key="popup" id="PopUp">
+                    <div ref={ref} className="bg-gray-100 dark:bg-slate-900 rounded-lg p-4 flex flex-col border-2 dark:border-white border-black min-w-[75vw] min-h-[75vh] opacity-100" key="popup" id="PopUp" aria-label='PopUp'>
                         <button className="ml-auto flex justify-end max-w-min" onClick={close}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-12 h-12 color-gray">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
