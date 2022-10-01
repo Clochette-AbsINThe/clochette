212969d9e8247eb1e177fa3529ba6066caf7bda9
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:46:45 +0200

    Add CRUD features for transaction model #19

---
+++ backend/app/crud/crud_transaction.py
@@ -0,0 +1,10 @@
+from app.crud.base import CRUDBase
+from app.models.transaction import Transaction
+from app.schemas.transaction import TransactionCreate, TransactionUpdate
+
+
+class CRUDTransaction(CRUDBase[Transaction, TransactionCreate, TransactionUpdate]):
+    ...
+
+
+transaction = CRUDTransaction(Transaction)
