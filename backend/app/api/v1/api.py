64117ab1907095054626fe4d8fe828db6426c7f7
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:50:50 +0200

    API v1 router #21

---
+++ backend/app/api/v1/api.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from importlib import import_module
+
+import app.api.v1.endpoints as endpoints
+
+endpoints_modules = [import_module('app.api.v1.endpoints.' + endpoint) for endpoint in endpoints.__all__]
+
+api_v1_router = APIRouter()
+
+for module in endpoints_modules:
+    api_v1_router.include_router(module.router, prefix="/{prefix}".format(prefix=module.__name__))
