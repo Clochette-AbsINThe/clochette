7baee8e64aac3718e728205180bd9e1b4f9f2be4
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:46:17 +0200

    Add CRUD features for out of stock model #19

---
+++ backend/app/crud/crud_out_of_stock.py
@@ -0,0 +1,10 @@
+from app.crud.base import CRUDBase
+from app.models.out_of_stock import OutOfStock
+from app.schemas.out_of_stock import OutOfStockCreate, OutOfStockUpdate
+
+
+class CRUDOutOfStock(CRUDBase[OutOfStock, OutOfStockCreate, OutOfStockUpdate]):
+    ...
+
+
+out_of_stock = CRUDOutOfStock(OutOfStock)
