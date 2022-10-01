2bf81ac0550605385a7c5e8d904a4e0d4df0e2ea
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:33:19 +0200

    Add drink pydantic validation scheme #19

---
+++ backend/app/schemas/drink.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class DrinkBase(BaseModel):
+    name: str
+
+
+class DrinkCreate(DrinkBase):
+    pass
+
+
+class DrinkUpdate(DrinkBase):
+    pass
+
+
+class Drink(DrinkBase):
+    id: int
+
+    class Config:
+        orm_mode = True
