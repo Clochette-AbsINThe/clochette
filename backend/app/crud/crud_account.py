8c018376fc0261f192a6507b70fde1ea6239936d
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:40:06 +0200

    Add CRUD features for account model

---
+++ backend/app/crud/crud_account.py
@@ -0,0 +1,10 @@
+from app.crud.base import CRUDBase
+from app.models.account import Account
+from app.schemas.account import AccountCreate, AccountUpdate
+
+
+class CRUDAccount(CRUDBase[Account, AccountCreate, AccountUpdate]):
+    ...
+
+
+account = CRUDAccount(Account)
