6350bb6c2a17b573923aaf0b6b838e3d2505190c
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:39:22 +0200

    Fix indentation issue #19

--- backend/app/crud/base.py
+++ backend/app/crud/base.py
@@ -28,38 +28,38 @@ class CRUDBase(
         """
         self.model = model
 
-        def read(self, db: Session, id: Any) -> Optional[ModelType]:
-            return db.query(self.model).filter(self.model.id == id).first()
+    def read(self, db: Session, id: Any) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == id).first()
 
-        def read_multi(self, db: Session, *, skip: int = 0, limit: int = 100) -> list[ModelType]:
-            return db.query(self.model).offset(skip).limit(limit).all()
+    def read_multi(self, db: Session, *, skip: int = 0, limit: int = 100) -> list[ModelType]:
+        return db.query(self.model).offset(skip).limit(limit).all()
 
-        def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
-            obj_in_data: jsonable_encoder(obj_in)
-            db_obj = self.model(**obj_in_data)
-            db.add(db_obj)
-            db.commit()
-            db.refresh(db_obj)
-            return db_obj
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
-        def update(self, db: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType | dict[str, Any]) -> ModelType:
-            obj_data = jsonable_encoder(db_obj)
-            if isinstance(obj_in, dict):
-                update_data = obj_in
-            else:
-                update_data = obj_in.dict(exclude_unset=True)
+    def update(self, db: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType | dict[str, Any]) -> ModelType:
+        obj_data = jsonable_encoder(db_obj)
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
 
-            for field in obj_data:
-                if field in update_data:
-                    setattr(db_obj, field, update_data[field])
+        for field in obj_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
 
-            db.add(db_obj)
-            db.commit()
-            db.refresh(db_obj)
-            return db_obj
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
-        def delete(self, db: Session, *, id: int) -> ModelType:
-            obj = db.query(self.model).get(id)
-            db.delete(obj)
-            db.commit()
-            return obj
+    def delete(self, db: Session, *, id: int) -> ModelType:
+        obj = db.query(self.model).get(id)
+        db.delete(obj)
+        db.commit()
+        return obj
