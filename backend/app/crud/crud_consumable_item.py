2e73c8e24122ebaa15b5de0a9902d64c123bbde9
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-01 19:40:18 +0200

    Add CRUD features for barrel model #19

---
+++ backend/app/crud/crud_consumable_item.py
@@ -0,0 +1,10 @@
+from app.crud.base import CRUDBase
+from app.models.consumable_item import ConsumableItem
+from app.schemas.consumable_item import ConsumableItemCreate, ConsumableItemUpdate
+
+
+class CRUDConsumableItem(CRUDBase[ConsumableItem, ConsumableItemCreate, ConsumableItemUpdate]):
+    ...
+
+
+consumable_item = CRUDConsumableItem(ConsumableItem)
