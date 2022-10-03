f67b89b9737efe9afd004b250338cae8b62bd4e3
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-03 18:01:40 +0200

    Fix header for the Docker #9

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -8,15 +8,8 @@ npm run build
 cd ..
 echo
 
-# Build the tar conf for Nginx
-echo "Create the tar conf for Nginx"
-echo
-cd docker/frontend || exit
-tar -czvf nginxconfig.io-clochette.dev.tar.gz ./nginxconfig.io-clochette.dev/
-cd ../
-echo
-
 # Build the docker image
 echo "Build the docker image"
 echo
+cd docker || exit
 docker compose -f "docker-compose.yml" up -d --build
