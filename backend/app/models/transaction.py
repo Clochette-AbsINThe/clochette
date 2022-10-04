8e75e93b8930c169a70e9d210702c6dc7a29cdb3
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-04 16:05:14 +0200

    Add bidirectionnal relationship between treasury and transaction models #19

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -16,11 +16,13 @@ class PaymentMethod(str, enum.Enum):
 
 class Transaction(Base):
     id = Column(Integer, primary_key=True, index=True)
-    treasury_id = Column(Integer, ForeignKey("treasury.id"))
     datetime = Column(DateTime, nullable=False)
     payment_method = Column(Enum(PaymentMethod), nullable=False)
     amount = Column(Float, nullable=False)
     sale = Column(Boolean, nullable=False) # True means selling, False means buying
+
+    treasury_id = Column(Integer, ForeignKey("treasury.id"))
+    treasury = relationship("Treasury", back_populates="transactions")
 
     barrels = relationship("Barrel", back_populates="transaction")
     glasses = relationship("Glass", back_populates="transaction")
--- backend/app/models/treasury.py
+++ backend/app/models/treasury.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, Float
+from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
@@ -7,3 +8,5 @@ class Treasury(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     total_amount = Column(Float, nullable=False)
     cash_amount = Column(Float, nullable=False)
+
+    transactions = relationship("Transaction", back_populates="treasury")
