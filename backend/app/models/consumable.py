16106bbf48f889623945c97277d9b89fa5a0c97d
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-04 16:27:26 +0200

    Refactor consumable.py

--- backend/app/models/consumable.py
+++ backend/app/models/consumable.py
@@ -6,9 +6,13 @@ from app.db.base_class import Base
 
 class Consumable(Base):
     id = Column(Integer, primary_key=True, nullable=False)
-    transaction_id_purchase = Column(Integer, ForeignKey("transaction.id"))
-    transaction_id_sale = Column(Integer, ForeignKey("transaction.id"))
-    consumable_item_id = Column(Integer, ForeignKey("consumableitem.id"))
-    stock_id = Column(Integer, ForeignKey("stock.id"))
     selling_price = Column(Float, nullable=False)
     buying_price = Column(Float, nullable=False)
+
+    consumable_item_id = Column(Integer, ForeignKey("consumableitem.id"))
+
+    stock_id = Column(Integer, ForeignKey("stock.id"))
+
+    transaction_id_purchase = Column(Integer, ForeignKey("transaction.id"))
+
+    transaction_id_sale = Column(Integer, ForeignKey("transaction.id"))
