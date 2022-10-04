95cfb228b5291d6aaa213ff5ed42283f09c0e204
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-04 16:06:41 +0200

    Refactor glass.py

--- backend/app/models/glass.py
+++ backend/app/models/glass.py
@@ -6,8 +6,8 @@ from app.db.base_class import Base
 
 class Glass(Base):
     id = Column(Integer, primary_key=True, nullable=False)
-    transaction_id = Column(Integer, ForeignKey("transaction.id"))
     barrel_id = Column(Integer, ForeignKey("barrel.id"))
     price = Column(Float, nullable=False)
 
+    transaction_id = Column(Integer, ForeignKey("transaction.id"))
     transaction = relationship("Transaction", back_populates="glasses")
