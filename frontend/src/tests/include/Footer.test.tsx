e27fda1738090f7e3065c7fab4d5bda8d1208b2e
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-18 00:40:25 +0200

    Mooving tests #18

--- frontend/src/tests/include/Footer.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Footer from '@include/Footer';
-
-test('Render footer', () => {
-    render(<Footer />);
-    const footer = screen.queryByText('Guillemet Samuel - Marzelleau Benoit');
-    expect(footer).toBeInTheDocument();
-});
--- frontend/src/tests/proxies/Config.test.ts
+++
@@ -1,5 +0,0 @@
-import BaseURL from '@proxies/Config';
-
-test('URL base is the goodd one', () => {
-    expect(BaseURL).toBe('https://clochette.dev/api/v1');
-});
