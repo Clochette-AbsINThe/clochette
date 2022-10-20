0b77c80e45d46ac9e6ea8065a6c9f05ec7526ae5
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-20 10:29:54 +0200

    Run migrations at application startup #20

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
+import psycopg2
+import shlex
+import subprocess
+
 from fastapi import FastAPI, APIRouter
+from time import sleep
 
 from app.api.v1.api import api_v1_router
 from app.core.config import settings
@@ -14,8 +19,65 @@ api_router = APIRouter(
 )
 
 
+@app.on_event("startup")
+async def run_migrations():
+    # Wait for db to start
+    while True:
+        try:
+            conn = psycopg2.connect(
+                host=settings.POSTGRES_HOST,
+                port=settings.POSTGRES_PORT,
+                user=settings.POSTGRES_USER,
+                password=settings.POSTGRES_PASSWORD,
+                database='postgres',
+            )
+            print("Postgres is up -- starting migrations")
+            # If some tables with enum type already exists in the database, migrations won't work: https://github.com/sqlalchemy/alembic/issues/278
+            # If this bug happen, it is necessary to clean up the database and then run migrations again.
+            # So let's clear the db and recreate all tables
+            print("Trying to drop {db} database if it exists...".format(db=settings.POSTGRES_DB))
+            cur = conn.cursor()
+            conn.autocommit = True # CREATE/DROP DATABASE cannot run inside a transaction block because it is irreversible
+            cur.execute(
+                "DROP DATABASE IF EXISTS {db};".format(
+                    db=settings.POSTGRES_DB
+                )
+            )
+            print("Database {db} dropped".format(db=settings.POSTGRES_DB))
+            print("Trying to create {db} database...".format(db=settings.POSTGRES_DB))
+            cur.execute(
+                "CREATE DATABASE {db};".format(
+                    db=settings.POSTGRES_DB
+                )
+            )
+            print("Database {db} created".format(db=settings.POSTGRES_DB))
+            conn.close()
+            # Run alembic migrations
+            print("Running migrations...")
+            subprocess.run(
+                shlex.split("alembic stamp head"),
+                check=True
+            )
+            subprocess.run(
+                shlex.split("alembic revision --autogenerate"),
+                check=True
+            )
+            subprocess.run(
+                shlex.split("alembic upgrade head"),
+                check=True
+            )
+            break
+        except psycopg2.OperationalError as e:
+            if e.pgcode is None or e.pgcode.startswith("08"):
+                print("Postgres is unavailable -- sleeping (receiving error code {code})".format(code=e.pgcode))
+                sleep(1)
+                continue
+            else:
+                raise e
+
+
 @api_router.get("/", status_code=200)
-def root() -> dict:
+async def root() -> dict:
     return {
         "msg": "Hello, World!",
     }
