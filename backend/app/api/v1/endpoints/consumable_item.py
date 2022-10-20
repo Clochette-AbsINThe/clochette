8f98af174f1eae46cfc696d064ed509ee7351700
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-20 23:57:32 +0200

    Optimize import and specify return type in consumable item endpoint #28

--- backend/app/api/v1/endpoints/consumable_item.py
+++ backend/app/api/v1/endpoints/consumable_item.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from typing import Any
 
 from app.crud.crud_consumable_item import consumable_item as consumable_items
 from app.dependencies import get_db
@@ -15,7 +14,7 @@ async def read_consumable_items(db=Depends(get_db)) -> list:
 
 
 @router.post("/", response_model=consumable_item_schema.ConsumableItem)
-async def create_consumable_item(consumable_item: consumable_item_schema.ConsumableItemCreate, db=Depends(get_db)):
+async def create_consumable_item(consumable_item: consumable_item_schema.ConsumableItemCreate, db=Depends(get_db)) -> dict:
     results = list(filter(lambda consumable_items_found: consumable_item.name.lower() == consumable_items_found.name.lower(), consumable_items.read_multi(db)))[:1] # TODO: Improve this
     if len(results):
         raise HTTPException(status_code=400, detail="Consumable item already exists")
