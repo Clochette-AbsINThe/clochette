38f666f08d580009e42cac5cfe0e949509ed8834
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-23 18:52:48 +0200

    Animation for 404 #12

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {}
+        extend: {
+            keyframes: {
+                'slide-in': {
+                    '0%': { transform: 'translateY(-10%)', opacity: 0 },
+                    '100%': { transform: 'translateX(0)', opacity: 1 }
+                }
+            },
+            animation: {
+                'slide-in': 'slide-in 0.9s forwards 0.8s'
+            }
+        }
     },
     plugins: [],
     darkMode: 'class'
