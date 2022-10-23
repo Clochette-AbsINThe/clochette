3ec7c5d3a24da4994afbb041485c1e3c1184b63f
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-23 18:56:16 +0200

    Add dynamic url change on useAxios #16

--- frontend/src/hooks/useAxios.ts
+++ frontend/src/hooks/useAxios.ts
@@ -7,7 +7,7 @@ interface ResponseValues<T> {
     response?: AxiosResponse<T> | null
 }
 
-type UseAxiosResult<T> = [ResponseValues<T>, (_config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>];
+type UseAxiosResult<T> = [ResponseValues<T>, (_config?: AxiosRequestConfig, _url?: string) => Promise<AxiosResponse<T>>];
 
 const API = axios.create({
     baseURL: 'https://clochette.dev/api/v1',
@@ -22,11 +22,11 @@ export default function useAxios<T>(url: string, _config?: AxiosRequestConfig): 
     const [error, setError] = useState<AxiosError | null>(null);
     const [response, setResponse] = useState<AxiosResponse<T> | null>(null);
 
-    const request = async (config?: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
+    const request = async (config?: AxiosRequestConfig, _url?: string): Promise<AxiosResponse<T>> => {
         setLoading(true);
         setError(null);
         try {
-            const response = await API.request({ ..._config, ...config, url });
+            const response = await API.request({ ..._config, ...config, url: _url ?? url });
             setResponse(response);
             return response;
         } catch (error: any) {
