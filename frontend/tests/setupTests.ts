8ff3a7b5fec2f7acd76c28a01fc44fc11c0679d7
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-23 19:13:31 +0200

    Add endpoints to mock test #18

--- frontend/tests/setupTests.ts
+++ frontend/tests/setupTests.ts
@@ -1,8 +1,11 @@
+import { removeIdFromUrl } from '@components/ConfigurationPage/ConfigurationPageBase';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+import { cleanup } from '@testing-library/react';
 
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import toast from 'react-hot-toast';
 
 export const date = new Date();
 
@@ -78,30 +81,15 @@ export const server = setupServer(
             ]
         }), ctx.status(200));
     }),
+    rest.post('https://clochette.dev/api/v1/Transaction/Buy', (req, res, ctx) => {
+        return res(ctx.json({}), ctx.status(200), ctx.delay(100));
+    }),
     rest.get('https://clochette.dev/api/v1/OutOfStockItem/Buy', (req, res, ctx) => {
         return res(ctx.json([
             {
                 id: 1,
                 name: 'EcoCup',
                 icon: 'Glass'
-            }]), ctx.status(200), ctx.delay(100));
-    }),
-    rest.post('https://clochette.dev/api/v1/Transaction/Buy', (req, res, ctx) => {
-        return res(ctx.json({}), ctx.status(200), ctx.delay(100));
-    }),
-    rest.get('https://clochette.dev/api/v1/drink', (req, res, ctx) => {
-        return res(ctx.json([
-            {
-                id: 1,
-                name: 'Rouge'
-            }]), ctx.status(200), ctx.delay(50));
-    }),
-    rest.get('https://clochette.dev/api/v1/ConsumableItem', (req, res, ctx) => {
-        return res(ctx.json([
-            {
-                id: 0,
-                name: 'Pizza',
-                icon: 'Food'
             }]), ctx.status(200), ctx.delay(100));
     }),
     rest.post('https://clochette.dev/api/v1/OutOfStockItem/Buy', (req, res, ctx) => {
@@ -111,21 +99,121 @@ export const server = setupServer(
             icon: 'Misc'
         }), ctx.status(200));
     }),
-    rest.post('https://clochette.dev/api/v1/ConsumableItem', (req, res, ctx) => {
+    rest.get('https://clochette.dev/api/v1/drink/', (req, res, ctx) => {
+        return res(ctx.json([
+            {
+                id: 1,
+                name: 'Rouge'
+            }]), ctx.status(200), ctx.delay(50));
+    }),
+    rest.get('https://clochette.dev/api/v1/drink/1', (req, res, ctx) => {
+        return res(ctx.json(
+            {
+                id: 1,
+                name: 'Rouge'
+            }), ctx.status(200), ctx.delay(50));
+    }),
+    rest.get('https://clochette.dev/api/v1/drink/10', (req, res, ctx) => {
+        return res(ctx.json(
+            {
+                id: 10,
+                name: 'NewDrink'
+            }), ctx.status(200), ctx.delay(50));
+    }),
+    rest.get('https://clochette.dev/api/v1/drink/404', (req, res, ctx) => {
+        return res(ctx.status(404), ctx.delay(50));
+    }),
+    rest.post('https://clochette.dev/api/v1/drink/', (req, res, ctx) => {
+        return res(ctx.json({
+            id: 10,
+            name: 'NewDrink'
+        }), ctx.status(200));
+    }),
+    rest.put('https://clochette.dev/api/v1/drink/:id', async (req, res, ctx) => {
+        const { id } = req.params;
+        const { name } = await req.json();
+        return await res(ctx.json({
+            id: Number(id),
+            name
+        }), ctx.status(200), ctx.delay(100));
+    }),
+    rest.delete('https://clochette.dev/api/v1/drink/1', (req, res, ctx) => {
+        return res(ctx.json({
+            id: 1,
+            name: 'Rouge'
+        }), ctx.status(200), ctx.delay(50));
+    }),
+    rest.get('https://clochette.dev/api/v1/consumable_item/', (req, res, ctx) => {
+        return res(ctx.json([
+            {
+                id: 0,
+                name: 'Pizza',
+                icon: 'Food'
+            }]), ctx.status(200), ctx.delay(100));
+    }),
+    rest.get('https://clochette.dev/api/v1/consumable_item/0', (req, res, ctx) => {
+        return res(ctx.json(
+            {
+                id: 0,
+                name: 'Pizza',
+                icon: 'Food'
+            }), ctx.status(200), ctx.delay(100));
+    }),
+    rest.get('https://clochette.dev/api/v1/consumable_item/10', (req, res, ctx) => {
         return res(ctx.json({
             id: 10,
             name: 'NewConsumable',
             icon: 'Misc'
         }), ctx.status(200));
     }),
-    rest.post('https://clochette.dev/api/v1/drink', (req, res, ctx) => {
+    rest.get('https://clochette.dev/api/v1/consumable_item/404', (req, res, ctx) => {
+        return res(ctx.status(404));
+    }),
+    rest.post('https://clochette.dev/api/v1/consumable_item/', (req, res, ctx) => {
         return res(ctx.json({
             id: 10,
-            name: 'NewDrink'
+            name: 'NewConsumable',
+            icon: 'Misc'
         }), ctx.status(200));
+    }),
+    rest.put('https://clochette.dev/api/v1/consumable_item/:id', async (req, res, ctx) => {
+        const { id } = req.params;
+        const { name, icon } = await req.json();
+        return await res(ctx.json({
+            id: Number(id),
+            name,
+            icon
+        }), ctx.status(200), ctx.delay(100));
+    }),
+    rest.delete('https://clochette.dev/api/v1/consumable_item/0', async (req, res, ctx) => {
+        return await res(ctx.json({
+            id: 0,
+            name: 'Pizza',
+            icon: 'Food'
+        }), ctx.status(200), ctx.delay(100));
     })
 );
 
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+    server.resetHandlers();
+    cleanup();
+    removeIdFromUrl();
+    toast.remove();
+});
 afterAll(() => server.close());
+
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn()
+    }))
+});
