ec69b72122795b3d7452fbd6ca6cf0b279ff5db6
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-23 19:00:21 +0200

    Add shadow #29

--- frontend/src/components/DropDownSelector.tsx
+++ frontend/src/components/DropDownSelector.tsx
@@ -87,7 +87,7 @@ export default function DropDownSelector(props: DropDownSelectorProps): JSX.Elem
 
 
     return (
-        <div className="flex m-2 items-center h-max rounded-xl bg-[#70707016] p-3 md:max-w-[33vw] max-w-full flex-wrap max-h-[88px]" ref={ref}>
+        <div className="flex m-2 items-center h-max rounded-xl bg-[#70707016] p-3 md:max-w-[33vw] max-w-full flex-wrap max-h-[88px] shadow-md" ref={ref}>
             <button className="btn-primary inline-flex items-center justify-between w-full my-2 text-xl lg:text-2xl" type="button" onClick={toggle} aria-label='dropdown-button'>
                 <span className='whitespace-nowrap overflow-hidden'>{'Ajouter ' + props.text}</span>
                 <svg className="ml-2 w-4 h-4" aria-hidden={!isSelected} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -113,7 +113,7 @@ export default function DropDownSelector(props: DropDownSelectorProps): JSX.Elem
                         />
                     </div>
                 </div>
-                <ul className="overflow-y-auto px-3 pb-3 max-h-48 text-sm text-gray-700 dark:text-gray-200 w-full">
+                <ul className="overflow-y-auto px-3 pb-3 max-h-[370px] text-sm text-gray-700 dark:text-gray-200 w-full">
                     {items.map((item) => (
                         <li
                             className='rounded hover:bg-gray-100 dark:hover:bg-gray-600 flex items-center cursor-pointer p-2'
@@ -144,7 +144,7 @@ export default function DropDownSelector(props: DropDownSelectorProps): JSX.Elem
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
                     </svg>
-                    {'Ajouter ' + props.text + ' manquant :'}
+                    {'Ajouter ' + props.text + ' manquant'}
                 </button>
             </div>
         </div>
