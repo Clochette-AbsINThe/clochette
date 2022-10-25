80422e044fb41fe1a273e5c941983b46a11efa9f
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-25 17:43:06 +0200

    Adapt out_of_stock_item validation model to front request body #19

--- backend/app/schemas/out_of_stock_item.py
+++ backend/app/schemas/out_of_stock_item.py
@@ -1,9 +1,38 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from app.core.types import IconName
 
 
 class OutOfStockItemBase(BaseModel):
-    name: str
-    sale: bool
+    name: str = Field(..., min_length=1)
+    icon: IconName
+
+
+class OutOfStockItemBuyCreateFront(OutOfStockItemBase):
+    pass
+
+
+class OutOfStockItemBuyCreate(OutOfStockItemBase):
+    buy_or_sell: bool
+    sell_price: int | None = None
+
+
+class OutOfStockItemBuyUpdateFront(OutOfStockItemBase):
+    pass
+
+
+class OutOfStockItemBuyUpdate(OutOfStockItemBase):
+    buy_or_sell: bool
+    sell_price: int | None = None
+
+
+class OutOfStockItemBuy(OutOfStockItemBase):
+    id: int
+    buy_or_sell: bool
+    sell_price: int | None
+
+    class Config:
+        orm_mode = True
 
 
 class OutOfStockItemCreate(OutOfStockItemBase):
