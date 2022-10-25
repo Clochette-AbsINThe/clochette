502b7aefa805d03cfed7900f0aded35a02431b33
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-25 17:42:00 +0200

    Improve out_of_stock_item SQLAlchemy model compliance #3

--- backend/app/models/out_of_stock_item.py
+++ backend/app/models/out_of_stock_item.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Enum, Integer, String
 from sqlalchemy.orm import relationship
 
+from app.core.types import IconName
 from app.db.base_class import Base
 
 
 class OutOfStockItem(Base):
     id = Column(Integer, primary_key=True, nullable=False)
+    buy_or_sell = Column(Boolean, nullable=False) # True means buy, False means sell
     name = Column(String, nullable=False)
-    sale = Column(Boolean, nullable=False)  # True is selling, False is buying
+    icon = Column(Enum(IconName), nullable=False)
+    sell_price = Column(Integer, nullable=True)
 
     outofstocks = relationship("OutOfStock", back_populates="item")
