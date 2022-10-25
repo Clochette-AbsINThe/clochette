85f1a2c175a0594f57608fcc38cf843b97ea7708
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-10-25 15:06:43 +0200

    Add endpoints for consummable items #28

--- backend/app/api/v1/endpoints/consumable_item.py
+++ backend/app/api/v1/endpoints/consumable_item.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from typing import Any
 
 from app.crud.crud_consumable_item import consumable_item as consumable_items
 from app.dependencies import get_db
@@ -15,7 +16,41 @@ async def read_consumable_items(db=Depends(get_db)) -> list:
 
 @router.post("/", response_model=consumable_item_schema.ConsumableItem)
 async def create_consumable_item(consumable_item: consumable_item_schema.ConsumableItemCreate, db=Depends(get_db)) -> dict:
-    results = list(filter(lambda consumable_items_found: consumable_item.name.lower() == consumable_items_found.name.lower(), consumable_items.read_multi(db)))[:1] # TODO: Improve this
+    results = list(filter(lambda consumable_items_found: consumable_item.name.lower(
+    ) == consumable_items_found.name.lower(), consumable_items.read_multi(db)))[:1]  # TODO: Improve this
     if len(results):
-        raise HTTPException(status_code=400, detail="Consumable item already exists")
+        raise HTTPException(
+            status_code=400, detail="Consumable item already exists")
     return consumable_items.create(db, obj_in=consumable_item)
+
+
+@router.get("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem)
+async def read_consumable_item(consumable_item_id: int, db=Depends(get_db)) -> Any:
+    consumable_item = consumable_items.read(db, consumable_item_id)
+    if consumable_item is None:
+        raise HTTPException(
+            status_code=404, detail="Consumable item not found")
+    return consumable_item
+
+
+@router.put("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem)
+async def update_consumable_item(consumable_item_id: int, consumable_item: consumable_item_schema.ConsumableItemUpdate, db=Depends(get_db)):
+    results = list(filter(lambda consumable_items_found: consumable_item.name.lower(
+    ) == consumable_items_found.name.lower(), consumable_items.read_multi(db)))[:1]  # TODO: Improve this
+    old_consumable_item = consumable_items.read(db, consumable_item_id)
+    if old_consumable_item is None:
+        raise HTTPException(
+            status_code=404, detail="Consumabel Item not found")
+    if len(results) and results[0].id != old_consumable_item.id:
+        raise HTTPException(
+            status_code=400, detail="Consumable item already exists")
+    return consumable_items.update(db, db_obj=old_consumable_item, obj_in=consumable_item)
+
+
+@router.delete("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem)
+async def delete_consumable_item(consumable_item_id: int, db=Depends(get_db)):
+    consumable_item = consumable_items.read(db, consumable_item_id)
+    if consumable_item is None:
+        raise HTTPException(
+            status_code=404, detail="Consumable Item not found")
+    return consumable_items.delete(db, id=consumable_item_id)
