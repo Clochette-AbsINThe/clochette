9708da7b34869bc9b009012e38fa5661dee6e450
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-26 16:15:06 +0200

    Conforming attributes to Python attribute naming conventions and adding validation schema #19

--- backend/app/schemas/barrel.py
+++ backend/app/schemas/barrel.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel, Field
 
 from app.core.types import IconName
+from app.schemas.drink import Drink
 
 
 class BarrelBase(BaseModel):
     empty: bool = False
     icon: IconName
     is_mounted: bool = False
-    sellPrice: float = Field(..., gt=0)
-    unitPrice: float = Field(..., gt=0)
+    sell_price: float = Field(..., gt=0)
+    unit_price: float = Field(..., gt=0)
 
 
 class BarrelCreate(BarrelBase):
@@ -17,6 +18,15 @@ class BarrelCreate(BarrelBase):
 
 class BarrelUpdate(BarrelBase):
     pass
+
+
+class BarrelInDB(BarrelBase):
+    id: int
+    drink_id: int
+    drink: Drink
+
+    class Config:
+        orm_mode = True
 
 
 class Barrel(BarrelBase):
