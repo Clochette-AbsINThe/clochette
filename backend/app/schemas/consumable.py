cfa48a7ddb4b5be54130da9b500db2424e5cb015
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-26 16:40:20 +0200

    Add attributes and a new validation schema to consumable pydantic validation schema #19

--- backend/app/schemas/consumable.py
+++ backend/app/schemas/consumable.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PrivateAttr
+
+from app.schemas.consumable_item import ConsumableItem
 
 
 class ConsumableBase(BaseModel):
-    selling_price: float
-    buying_price: float
+    unit_price: float = Field(..., gt=0)
+    sell_price: float = Field(..., gt=0)
+    empty: bool
 
 
 class ConsumableCreate(ConsumableBase):
@@ -16,12 +19,15 @@ class ConsumableUpdate(ConsumableBase):
 
 class Consumable(ConsumableBase):
     id: int
-    transaction_id_purchase: int
-    transaction_id_sale: int
     consumable_item_id: int
-    stock_id: int
-    selling_price: float
-    buying_price: float
+    _consumable_item: ConsumableItem = PrivateAttr(default_factory=ConsumableItem)
+    name: str
+    icon: str
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.name = self._consumable_item.default_factory().name
+        self.icon = self._consumable_item.default_factory().icon
 
     class Config:
         orm_mode = True
