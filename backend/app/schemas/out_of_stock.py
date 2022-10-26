864d2f07332e68f22e516cf4194c7769d22d3d16
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-10-26 17:06:27 +0200

    Add validation schemas to out_of_stock to extract data from other linked models #19

--- backend/app/schemas/out_of_stock.py
+++ backend/app/schemas/out_of_stock.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PrivateAttr
 
+from app.schemas.out_of_stock_item import OutOfStockItemBuy, OutOfStockItemSell
 
 class OutOfStockBase(BaseModel):
-    unit_price: float
+    unit_price: float = Field(..., gt=0)
 
 
 class OutOfStockCreate(OutOfStockBase):
@@ -13,10 +14,35 @@ class OutOfStockUpdate(OutOfStockBase):
     pass
 
 
-class OutOfStock(OutOfStockBase):
+class OutOfStockBuy(OutOfStockBase):
     id: int
-    transaction_id: int
     item_id: int
+    _item: OutOfStockItemBuy = PrivateAttr(default_factory=OutOfStockItemBuy)
+    name: str
+    icon: str
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.name = self._item.default_factory().name
+        self.icon = self._item.default_factory().icon
+
+    class Config:
+        orm_mode = True
+
+
+class OutOfStockSell(OutOfStockBase):
+    id: int
+    item_id: int
+    _item: OutOfStockItemSell = PrivateAttr(default_factory=OutOfStockItemSell)
+    name: str
+    icon: str
+    sell_price: float = Field(..., gt=0)
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.name = self._item.default_factory().name
+        self.icon = self._item.default_factory().icon
+        self.sell_price = self._item.default_factory().sell_price
 
     class Config:
         orm_mode = True
