fbd0a59cbde149e8e69e68cb0f1353b5a498fabd
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-11-13 19:55:25 +0100

    Setup nextJS #38

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             keyframes: {
