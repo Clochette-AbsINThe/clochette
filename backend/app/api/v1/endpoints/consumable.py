079116213c130bf0e83851ee14101be6c9cc7816
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-15 00:47:21 +0100

    Add empty=False filter to consumable endpoints and add one to get them all #28

--- backend/app/api/v1/endpoints/consumable.py
+++ backend/app/api/v1/endpoints/consumable.py
@@ -10,9 +10,14 @@ router = APIRouter()
 
 @router.get("/", response_model=list[consumable_schema.Consumable])
 async def read_consumables(db=Depends(get_db)) -> list:
-    return consumables.query(db, limit=None)
+    return consumables.query(db, empty=False, limit=None)
 
 
 @router.get("/distincts/", response_model=list[consumable_schema.Consumable])
 async def read_consumables_distincts(db=Depends(get_db)) -> list:
-    return consumables.query(db, distinct='consumable_item_id', limit=None)
+    return consumables.query(db, distinct='consumable_item_id', empty=False, limit=None)
+
+
+@router.get("/all/", response_model=list[consumable_schema.Consumable])
+async def read_all_consumables(db=Depends(get_db)) -> list:
+    return consumables.query(db, limit=None)
