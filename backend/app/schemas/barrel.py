e4107ee01913c65db1c041cdc01447f7ed836f33
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-14 15:22:22 +0100

    Update barrel validation scheme #19

--- backend/app/schemas/barrel.py
+++ backend/app/schemas/barrel.py
@@ -1,14 +1,12 @@
-from pydantic import Field
+from pydantic import Field, validator
 
 from app.core.config import DefaultModel
-from app.core.types import IconName
 from app.schemas.drink import Drink
 
 
 class BarrelBase(DefaultModel):
     drink_id: int | None = Field(..., alias='fkId')
     empty: bool = False
-    icon: IconName
     is_mounted: bool = False
     sell_price: float = Field(..., gt=0)
     unit_price: float = Field(..., gt=0)
@@ -24,6 +22,12 @@ class BarrelUpdate(BarrelBase):
 
 class Barrel(BarrelBase):
     id: int
+    drink: Drink | None = Field(..., exclude=True)
+    name: str | None
+
+    @validator('name', always=True)
+    def populate_name(cls, v, values):
+        return values['drink'].name
 
     class Config:
         orm_mode = True
