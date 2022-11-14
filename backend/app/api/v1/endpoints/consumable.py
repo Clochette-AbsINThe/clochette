1f5fb321e5758e5bd0e6eb6751c7c66129788bee
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-15 00:43:08 +0100

    Use query method instead of read_distincts that doesn't exist anymore in consumable endpoint #28

--- backend/app/api/v1/endpoints/consumable.py
+++ backend/app/api/v1/endpoints/consumable.py
@@ -15,4 +15,4 @@ async def read_consumables(db=Depends(get_db)) -> list:
 
 @router.get("/distincts/", response_model=list[consumable_schema.Consumable])
 async def read_consumables_distincts(db=Depends(get_db)) -> list:
-    return consumables.read_distincts(db, 'consumable_item_id', limit=None)
+    return consumables.query(db, distinct='consumable_item_id', limit=None)
