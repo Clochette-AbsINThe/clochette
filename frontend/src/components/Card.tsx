2ec96fb741764dc2420216e3b7f38e84e720498f
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-11-15 12:52:19 +0100

    Add more style to card #34

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -15,23 +15,22 @@ export default function Card(props: CardProps): JSX.Element {
 
     return (
         <div className='w-full p-4 flex-grow'>
-            <div className='border space-y-5 p-4 bg-gray-50 rounded-lg shadow-md dark:bg-gray-800 h-full flex flex-col justify-between'>
-                <div className='flex flex-row items-center'>
-                    <div className='flex-shrink pr-4'>{icon.map((name) => getIcon(name))}</div>
-                    <div className='flex-1 text-right md:text-center space-y-3'>
-                        <h3 className='text-3xl font-bold uppercase'>{title}</h3>
-                        <h5 className='text-2xl font-semibold text-gray-500 dark:text-gray-100'>{description}</h5>
+            <Link href={link}>
+                <div className='border p-4 bg-gray-50 rounded-lg shadow-md dark:bg-gray-800 h-full flex flex-col justify-between cursor-pointer'>
+                    <div className='flex flex-row items-center'>
+                        <div className='flex-shrink pr-4'>{icon.map((name) => getIcon(name))}</div>
+                        <div className='flex-1 text-right md:text-center space-y-3'>
+                            <h3 className='text-3xl font-bold uppercase'>{title}</h3>
+                            <h5 className='text-2xl font-semibold text-gray-500 dark:text-gray-100'>{description}</h5>
+                        </div>
                     </div>
-                </div>
-                <div className='flex flex-row items-center'>
-                    <div className='flex-grow'></div>
-                    <div className='flex-shrink p-4'>
+                    <div className='flex flex-row items-center p-4 lg:mt-16'>
                         <Link href={link}>
-                            <a className='btn-primary'>{buttonLabel ?? 'Voir'}</a>
+                            <a className='btn-primary w-full'>{buttonLabel ?? 'Voir'}</a>
                         </Link>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 }
