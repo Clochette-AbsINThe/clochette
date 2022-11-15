e6f758506b39e57bfe40b0cfd312963ab76e0742
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-11-15 01:09:20 +0100

    Adapt endpoints for stock page #42

--- frontend/src/utils/endpoints.ts
+++ frontend/src/utils/endpoints.ts
@@ -8,7 +8,9 @@ export const endpoints = {
         outOfStockItemBuy: '/out_of_stock_item/buy/',
         outOfStockItemSell: '/out_of_stock_item/sell/',
         outOfStockItem: '/out_of_stock_item/',
-        mountedBarrel: '/barrel/mounted/',
+        barrel: '/barrel/',
+        barrelMounted: '/barrel/mounted/',
+        barrelDistinct: '/barrel/distincts/',
         transaction: '/transaction/',
         transactionSell: '/transaction/sell/',
         transactionBuy: '/transaction/buy/'
