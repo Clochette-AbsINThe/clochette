f85444c6b3dc0f82176d46e064edd752ca133c59
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-17 23:27:45 +0100

    Run alert_backend function as a background task #24

--- backend/app/core/middleware.py
+++ backend/app/core/middleware.py
@@ -1,11 +1,13 @@
+from collections.abc import Callable
 from fastapi.requests import Request
 from fastapi.responses import Response
 from fastapi import status
+from starlette.background import BackgroundTask
 from starlette.middleware.base import BaseHTTPMiddleware
 
 
 class ExceptionMonitorMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, alert_backend: callable):
+    def __init__(self, app, alert_backend: Callable[[Exception, dict], None]):
         super().__init__(app)
         self.alert_backend = alert_backend
     
@@ -26,14 +28,17 @@ class ExceptionMonitorMiddleware(BaseHTTPMiddleware):
             return await call_next(request)
         except Exception as e:
             body = await self.get_body(request)
-            self.alert_backend(
-                exception=e,
+            print(type(self.alert_backend))
+            task = BackgroundTask(
+                self.alert_backend,
+                e,
                 method=request.method,
                 url=request.url,
                 headers=request.headers,
                 body=body,
             )
-        return Response(
-            "Internal Server Error, administrator has been notified",
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+            return Response(
+                content="Internal Server Error, admin has been notified",
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                background=task
+            )
