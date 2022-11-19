e92cbe1846d8977d1cfc4282a2948896c68ffef5
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-19 23:26:38 +0100

    Complete auth endpoints #22

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -3,14 +3,15 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Any
 
 from app.core.auth import authenticate, create_access_token
-from app.crud.crud_account import account as accounts
+from app.dependencies import get_current_account
 from app.schemas import account as account_schema
+from app.schemas import token as token_schema
 
 
 router = APIRouter()
 
 
-@router.post("/login")
+@router.post("/login", response_model=token_schema.Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     account = authenticate(username=form_data.username, password=form_data.password)
     if account is None:
@@ -23,3 +24,8 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
         "access_token": create_access_token(subject=account.id),
         "token_type": "bearer",
     }
+
+
+@router.get("/me", response_model=account_schema.Account)
+def read_account_me(current_account: account_schema.Account = Depends(get_current_account)) -> Any:
+    return current_account
