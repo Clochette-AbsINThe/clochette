fb0f037ffe39db80ae505ec2155f4d578fa7bd57
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-20 19:22:05 +0100

    Authenticate in endpoint, not in external function #22

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -2,8 +2,9 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Any
 
-from app.core.auth import authenticate, create_access_token
-from app.dependencies import get_current_account
+from app.core.security import create_access_token, verify_password
+from app.crud.crud_account import account as accounts
+from app.dependencies import get_current_account, get_db
 from app.schemas import account as account_schema
 from app.schemas import token as token_schema
 
@@ -12,9 +13,9 @@ router = APIRouter()
 
 
 @router.post("/login", response_model=token_schema.Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
-    account = authenticate(username=form_data.username, password=form_data.password)
-    if account is None:
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db = Depends(get_db)) -> Any:
+    account = accounts.query(db, username=form_data.username, limit=1)[0]
+    if account is None or not verify_password(form_data.password, account.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials",
