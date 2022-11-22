b5ed1adb45c445b6f52615bd0c23f5b14c56e1cc
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-23 00:59:12 +0100

    Don't protect all the endpoints of a module simultaneously #22

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,22 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from importlib import import_module
 
 import app.api.v1.endpoints as endpoints
-from app.dependencies import get_current_account
+
 
 endpoints_modules = [import_module('app.api.v1.endpoints.' + endpoint) for endpoint in endpoints.__all__]
 
 api_v1_router = APIRouter()
 
+
 for module in endpoints_modules:
-    if getattr(module, 'AUTHENTICATION_REQUIRED', None):
-        api_v1_router.include_router(
-            module.router,
-            prefix="/{prefix}".format(prefix=module.__name__.split('.')[-1]),
-            dependencies=[Depends(get_current_account)] , # Protect all endpoints by default
-        )
-    else:
-        api_v1_router.include_router(
-            module.router,
-            prefix="/{prefix}".format(prefix=module.__name__.split('.')[-1]),
-        )
+    api_v1_router.include_router(
+        module.router,
+        prefix="/{prefix}".format(prefix=module.__name__.split('.')[-1]),
+    )
