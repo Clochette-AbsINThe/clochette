9fbe40028a8f85d32e7130b11ecfcd2448c97fe2
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-23 01:00:37 +0100

    Use route dependencies to enable authentication for consumable item endpoints #22

--- backend/app/api/v1/endpoints/consumable_item.py
+++ backend/app/api/v1/endpoints/consumable_item.py
@@ -3,20 +3,19 @@ from typing import Any
 
 from app.crud.crud_consumable_item import consumable_item as consumable_items
 from app.crud.crud_consumable import consumable as consumables
-from app.dependencies import get_db
+from app.dependencies import get_current_account, get_db
 from app.schemas import consumable_item as consumable_item_schema
 
 
 router = APIRouter()
-AUTHENTICATION_REQUIRED = True
 
 
-@router.get("/", response_model=list[consumable_item_schema.ConsumableItem])
+@router.get("/", response_model=list[consumable_item_schema.ConsumableItem], dependencies=[Depends(get_current_account)])
 async def read_consumable_items(db=Depends(get_db)) -> list:
     return consumable_items.read_multi(db)
 
 
-@router.post("/", response_model=consumable_item_schema.ConsumableItem)
+@router.post("/", response_model=consumable_item_schema.ConsumableItem, dependencies=[Depends(get_current_account)])
 async def create_consumable_item(consumable_item: consumable_item_schema.ConsumableItemCreate, db=Depends(get_db)) -> dict:
     if consumable_items.query(db, name=consumable_item.name, limit=1):
         raise HTTPException(
@@ -26,7 +25,7 @@ async def create_consumable_item(consumable_item: consumable_item_schema.Consuma
     return consumable_items.create(db, obj_in=consumable_item)
 
 
-@router.get("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem)
+@router.get("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem, dependencies=[Depends(get_current_account)])
 async def read_consumable_item(consumable_item_id: int, db=Depends(get_db)) -> Any:
     consumable_item = consumable_items.read(db, consumable_item_id)
     if consumable_item is None:
@@ -37,7 +36,7 @@ async def read_consumable_item(consumable_item_id: int, db=Depends(get_db)) -> A
     return consumable_item
 
 
-@router.put("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem)
+@router.put("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem, dependencies=[Depends(get_current_account)])
 async def update_consumable_item(consumable_item_id: int, consumable_item: consumable_item_schema.ConsumableItemUpdate, db=Depends(get_db)):
     old_consumable_item = consumable_items.read(db, consumable_item_id)
     if old_consumable_item is None:
@@ -54,7 +53,7 @@ async def update_consumable_item(consumable_item_id: int, consumable_item: consu
     return consumable_items.update(db, db_obj=old_consumable_item, obj_in=consumable_item)
 
 
-@router.delete("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem)
+@router.delete("/{consumable_item_id}", response_model=consumable_item_schema.ConsumableItem, dependencies=[Depends(get_current_account)])
 async def delete_consumable_item(consumable_item_id: int, db=Depends(get_db)):
     if consumable_items.read(db, consumable_item_id) is None:
         raise HTTPException(
