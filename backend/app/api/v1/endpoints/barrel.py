f26830292a693915cca7314dbe7a997f11c26df6
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-23 01:00:14 +0100

    Use route dependencies to enable authentication for barrel endpoints #22

--- backend/app/api/v1/endpoints/barrel.py
+++ backend/app/api/v1/endpoints/barrel.py
@@ -1,35 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.crud.crud_barrel import barrel as barrels
-from app.dependencies import get_db
+from app.dependencies import get_current_account, get_db
 from app.schemas import barrel as barrel_schema
 
 
 router = APIRouter()
-AUTHENTICATION_REQUIRED = True
 
 
-@router.get("/", response_model=list[barrel_schema.Barrel])
+@router.get("/", response_model=list[barrel_schema.Barrel], dependencies=[Depends(get_current_account)])
 async def read_barrels(db=Depends(get_db)) -> list:
     return barrels.query(db, empty=False, limit=None)
 
 
-@router.get("/mounted/", response_model=list[barrel_schema.Barrel])
+@router.get("/mounted/", response_model=list[barrel_schema.Barrel], dependencies=[Depends(get_current_account)])
 async def read_mounted_barrels(db=Depends(get_db)) -> list:
     return barrels.query(db, is_mounted=True, empty=False, limit=None)
 
 
-@router.get("/distincts/", response_model=list[barrel_schema.Barrel])
+@router.get("/distincts/", response_model=list[barrel_schema.Barrel], dependencies=[Depends(get_current_account)])
 async def read_distincts_barrels(db=Depends(get_db)) -> list:
     return barrels.query(db, distinct='drink_id', empty=False, limit=None)
 
 
-@router.get("/all/", response_model=list[barrel_schema.Barrel])
+@router.get("/all/", response_model=list[barrel_schema.Barrel], dependencies=[Depends(get_current_account)])
 async def read_all_barrels(db=Depends(get_db)) -> list:
     return barrels.query(db, limit=None)
 
 
-@router.put("/{barrel_id}", response_model=barrel_schema.Barrel)
+@router.put("/{barrel_id}", response_model=barrel_schema.Barrel, dependencies=[Depends(get_current_account)])
 async def update_barrel(barrel_id: int, barrel: barrel_schema.BarrelUpdate, db=Depends(get_db)) -> barrel_schema.Barrel:
     db_barrel = barrels.read(db, barrel_id)
     if db_barrel is None:
