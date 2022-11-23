42b4d9bd20e2df80e22ef862506285b4f932b1e6
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-23 01:25:53 +0100

    Fix bad dependency in glass and out of stock endpoints #22

--- backend/app/api/v1/endpoints/glass.py
+++ backend/app/api/v1/endpoints/glass.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.crud.crud_glass import glass as glasses
-from app.dependencies import get_db
+from app.dependencies import get_current_account, get_db
 from app.schemas import glass as glass_schema
 
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[glass_schema.Glass], dependencies=[Depends(get_db)])
+@router.get("/", response_model=list[glass_schema.Glass], dependencies=[Depends(get_current_account)])
 async def read_glasses(db=Depends(get_db)) -> list:
     return glasses.query(db, limit=None)
