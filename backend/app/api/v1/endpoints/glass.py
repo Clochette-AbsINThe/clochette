d6080c7ea6582d30d9946877bd00da2f2e43f253
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-23 01:00:58 +0100

    Use route dependencies to enable authentication for glass endpoints #22

--- backend/app/api/v1/endpoints/glass.py
+++ backend/app/api/v1/endpoints/glass.py
@@ -6,9 +6,8 @@ from app.schemas import glass as glass_schema
 
 
 router = APIRouter()
-AUTHENTICATION_REQUIRED = True
 
 
-@router.get("/", response_model=list[glass_schema.Glass])
+@router.get("/", response_model=list[glass_schema.Glass], dependencies=[Depends(get_db)])
 async def read_glasses(db=Depends(get_db)) -> list:
     return glasses.query(db, limit=None)
