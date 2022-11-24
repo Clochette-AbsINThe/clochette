9841b0b96a15129e0634f538bbf7d829d3699956
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-11-24 02:23:23 +0100

    Update for Navbar #18

--- frontend/tests/include/Navbar.test.tsx
+++ frontend/tests/include/Navbar.test.tsx
@@ -8,30 +8,11 @@ test('Render Navbar', () => {
     expect(navbar).toBeInTheDocument();
 });
 
-test('Resize Navbar Hide', () => {
-    window.innerWidth = 500;
-    render(<Navbar />);
-    const login = screen.queryByText('Se connecter');
-    expect(login).toBeNull();
-});
-
 test('Resize Navbar Show', () => {
     window.innerWidth = 1025;
     render(<Navbar />);
     const login = screen.queryByText('Se connecter');
     expect(login).toBeInTheDocument();
-});
-
-test('debounce Resize Navbar Show', async () => {
-    window.innerWidth = 1024;
-    render(<Navbar />);
-    await act(async () => {
-        window.innerWidth = 760;
-        window.dispatchEvent(new Event('resize'));
-        await new Promise((resolve) => setTimeout(resolve, 200));
-    });
-    const login = screen.queryByText('Se connecter');
-    expect(login).toBeNull();
 });
 
 test('Open hamburger-menu', async () => {
