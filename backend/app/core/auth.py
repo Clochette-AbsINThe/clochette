300a0c63c9ee120aa9b7003d9b163ad9cd2ebfca
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-11-28 14:33:32 +0100

    Add trailing slash to oauth2 scheme endpoint

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -3,4 +3,4 @@ from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f'{settings.API_V1_PREFIX}/auth/login') # TODO: Shouldn't be hardcoded
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f'{settings.API_V1_PREFIX}/auth/login/') # TODO: Shouldn't be hardcoded
