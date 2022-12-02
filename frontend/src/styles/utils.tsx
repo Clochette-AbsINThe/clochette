2f571b98881c1b1cf2137ddf746fefe6e45a7432
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-12-02 15:45:08 +0100

    Add Virement in icon

--- frontend/src/styles/utils.tsx
+++ frontend/src/styles/utils.tsx
@@ -201,6 +201,18 @@ const CBIcon = (props: IconProps): JSX.Element => {
     );
 };
 
+const VirementIcon = (props: IconProps): JSX.Element => {
+    return (
+        <svg
+            className={props.className}
+            viewBox='0 0 24 24'>
+            <path
+                fill='currentColor'
+                d='M15 14v-3h3V9l4 3.5l-4 3.5v-2h-3m-1-6.3V9H2V7.7L8 4l6 3.7M7 10h2v5H7v-5m-4 0h2v5H3v-5m10 0v2.5l-2 1.8V10h2m-3.9 6l-.6.5l1.7 1.5H2v-2h7.1m7.9-1v3h-3v2l-4-3.5l4-3.5v2h3Z'></path>
+        </svg>
+    );
+};
+
 const SettingsIcon = (props: IconProps): JSX.Element => {
     return (
         <svg
@@ -280,6 +292,13 @@ export function getIcon(name: IconName, className: string = 'w-10 h-10 dark:text
                 <CBIcon
                     className={className}
                     key='CB'
+                />
+            );
+        case 'Virement':
+            return (
+                <VirementIcon
+                    className={className}
+                    key='Virement'
                 />
             );
         case 'Setting':
