383333b8763a94cf7b6c75bca3d03383611487d1
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-12-02 15:53:10 +0100

    Add various exemple of charts to dashboard #72

--- frontend/src/pages/account/dashboard.tsx
+++ frontend/src/pages/account/dashboard.tsx
@@ -7,7 +7,23 @@ const StackedChartsPage: NextPage = () => {
         <Base
             title='Dashboard'
             description=''>
-            <StackedCharts />
+            <>
+                <StackedCharts
+                    groupByCallback={(item) => new Date(item.datetime).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })}
+                    sortCallback={(a: string, b: string) => (a > b ? 1 : -1)}
+                    queryFilter='te'
+                />
+                <StackedCharts
+                    groupByCallback={(item) => new Date(item.datetime).toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' })}
+                    sortCallback={(a: string, b: string) => new Date(a).getTime() - new Date(b).getTime()}
+                    queryFilter='te'
+                />
+                <StackedCharts
+                    groupByCallback={(item) => item.paymentMethod}
+                    sortCallback={(a: string, b: string) => a.localeCompare(b)}
+                    queryFilter='te'
+                />
+            </>
         </Base>
     );
 };
