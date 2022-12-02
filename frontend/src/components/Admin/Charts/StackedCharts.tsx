37f286dcfaef1edf37b2b652df4fe88b14195dd0
SamuelGuillemet <samu.guillemet0@gmail.com>
2022-12-02 15:28:33 +0100

    Make StackedChart generic #72

--- frontend/src/components/Admin/Charts/StackedCharts.tsx
+++ frontend/src/components/Admin/Charts/StackedCharts.tsx
@@ -4,7 +4,7 @@ import type { ChartOptions } from 'chart.js';
 import { useEffect, useState } from 'react';
 import { getTransactionItems } from '@proxies/DashboardProxies';
 import { ItemTransactionResponse, TransactionType } from '@types';
-import { unique } from '@utils/utils';
+import { rainbowColors, groupBy, unique } from '@utils/utils';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const options: ChartOptions = {
@@ -19,12 +19,24 @@ const options: ChartOptions = {
     },
     plugins: {
         legend: {
-            position: 'left'
+            position: 'left',
+            fullSize: true
+        },
+        tooltip: {
+            enabled: true
         }
     }
 };
 
-export default function StackedCharts() {
+interface StackedChartProps<K> {
+    groupByCallback: (item: TransactionType<ItemTransactionResponse>) => K;
+    sortCallback: (a: K, b: K) => number;
+    queryFilter: string;
+}
+
+export default function StackedCharts<K>(props: StackedChartProps<K>) {
+    const { groupByCallback, sortCallback, queryFilter } = props;
+
     const [transactions, setTransactions] = useState<Array<TransactionType<ItemTransactionResponse>>>([]);
     const [getData, { loading }] = getTransactionItems(setTransactions);
 
@@ -37,13 +49,24 @@ export default function StackedCharts() {
         .flat()
         .filter(unique);
 
+    const testDatasets = groupBy(transactions, groupByCallback, sortCallback);
+
     const data: ChartData<'bar'> = {
-        labels: transactions.map((item) => new Date(item.datetime).getMinutes()),
-        datasets: names.map((name) => {
+        labels: Array.from(testDatasets.keys()),
+        datasets: names.sort().map((name, index) => {
             return {
                 label: name,
-                data: transactions.map((item) => item.items.find((item) => item.item.name === name)?.quantity || 0),
-                backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`
+                data: Array.from(testDatasets.values()).map((transactionArrayForAGivenFilter) =>
+                    transactionArrayForAGivenFilter
+                        .map((transaction) =>
+                            transaction.items
+                                .filter((item) => item.item.name === name)
+                                .map((item) => item.quantity)
+                                .reduce((a, b) => a + b, 0)
+                        )
+                        .reduce((a, b) => a + b, 0)
+                ),
+                backgroundColor: rainbowColors(names.length + 2, index)
             };
         })
     };
@@ -51,11 +74,20 @@ export default function StackedCharts() {
     if (loading) return <div>Loading...</div>;
 
     return (
-        <Bar
-            data={data}
-            height={400}
-            width={600}
-            options={options as any}
-        />
+        <>
+            <button
+                className='btn-primary max-w-max'
+                onClick={getData}>
+                Reload data
+            </button>
+            <div className='max-w-[40rem] max-h-[15rem]'>
+                <Bar
+                    data={data}
+                    height={400}
+                    width={600}
+                    options={options as any}
+                />
+            </div>
+        </>
     );
 }
