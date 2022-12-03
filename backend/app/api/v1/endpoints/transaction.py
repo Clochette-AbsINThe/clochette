4e350b6ba8ac78a9fc68a47e9fd41f0de2903ce5
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-12-03 13:00:17 +0100

    Add query parameters support in transaction endpoint #86

--- backend/app/api/v1/endpoints/transaction.py
+++ backend/app/api/v1/endpoints/transaction.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.core.translation import Translator
+from app.core.utils.misc import to_query_parameters
 from app.crud.crud_transaction import transaction as transactions
 from app.dependencies import get_current_account, get_db
 from app.schemas import transaction as transaction_schema
@@ -11,8 +12,8 @@ translator = Translator(element="transaction")
 
 
 @router.get("/", response_model=list[transaction_schema.Transaction], dependencies=[Depends(get_current_account)])
-async def read_transactions(db=Depends(get_db)) -> list[transaction_schema.Transaction]:
-    return transactions.read_multi(db)
+async def read_transactions(db=Depends(get_db), query=Depends(to_query_parameters(transaction_schema.TransactionBase))) -> list[transaction_schema.Transaction]:
+    return transactions.query(db, limit=None, **query.dict(exclude_none=True, exclude_unset=True))
 
 
 @router.post("/", response_model=transaction_schema.Transaction, dependencies=[Depends(get_current_account)])
