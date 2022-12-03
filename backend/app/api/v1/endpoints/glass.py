e80606381fda058980b0d1520450574fddf1b20e
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-12-03 12:59:30 +0100

    Add query parameters support in glass endpoint #86

--- backend/app/api/v1/endpoints/glass.py
+++ backend/app/api/v1/endpoints/glass.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
+from app.core.utils.misc import to_query_parameters
 from app.crud.crud_glass import glass as glasses
 from app.dependencies import get_current_account, get_db
 from app.schemas import glass as glass_schema
@@ -9,5 +10,5 @@ router = APIRouter(tags=["glass"])
 
 
 @router.get("/", response_model=list[glass_schema.Glass], dependencies=[Depends(get_current_account)])
-async def read_glasses(db=Depends(get_db)) -> list:
-    return glasses.query(db, limit=None)
+async def read_glasses(db=Depends(get_db), query=Depends(to_query_parameters(glass_schema.GlassBase))) -> list:
+    return glasses.query(db, limit=None, **query.dict(exclude_none=True, exclude_unset=True))
