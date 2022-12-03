44cd06fa75a8e60c24e5007143620ad579de2990
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-12-03 03:19:17 +0100

    Optimize imports in account SQLAlchemy model

--- backend/app/models/account.py
+++ backend/app/models/account.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, Float, String
+from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
