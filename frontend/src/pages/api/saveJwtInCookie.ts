63714e7a09782b9e4d888c5e9fcc20ea6efb1fa0
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2022-12-15 00:46:11 +0100

    🐛 FIX: `ERR_HTTP_HEADERS_SENT` error on saveJwtInCookie

--- frontend/src/pages/api/saveJwtInCookie.ts
+++ frontend/src/pages/api/saveJwtInCookie.ts
@@ -8,7 +8,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         const decodedJWT = parseJwt(jwt);
         res.setHeader('Set-Cookie', `jwt=${jwt}; Path=/; HttpOnly; SameSite=Lax; Secure; Max-Age=${decodedJWT.exp - decodedJWT.iat};`);
     } catch {
-        res.status(400).json({ error: 'Invalid JWT' });
+        return res.status(400).json({ error: 'Invalid JWT' });
     }
     // This redirect is used to prevent the browser from displaying the JSON response and to force the setting of the cookie
     res.redirect(302, '/login');
