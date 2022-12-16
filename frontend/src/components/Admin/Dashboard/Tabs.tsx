a8211e9623d2692f2344dd68d2b63633a7a232fc
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2022-12-16 22:08:33 +0100

    👌 IMPROVE: Add roles to access pages in account

--- frontend/src/components/Admin/Dashboard/Tabs.tsx
+++ frontend/src/components/Admin/Dashboard/Tabs.tsx
@@ -1,24 +1,28 @@
+import { useAuthContext } from '@components/Context';
+import { links } from '@pages/account/[[...page]]';
+import { parseJwt } from '@utils/utils';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
-const links = [
-    { label: 'Dashboard', to: '/account/dashboard' },
-    { label: 'Users', to: '/account/users' },
-    { label: 'Products', to: '/account/products' }
-];
 
-export default function Tabs() {
-    const router = useRouter();
-    const { pathname } = router;
+export default function Tabs({ pathname }: { pathname: string }) {
+    const activeLink = links.find((link) => link.to.search(pathname) !== -1);
+    let filteredLinks = [] as typeof links;
+
+    const { jwt } = useAuthContext();
+    if (jwt) {
+        const userRoles = parseJwt(jwt)!.roles ?? [];
+        filteredLinks = links.filter((link) => link.role.some((role) => userRoles.includes(role)));
+        filteredLinks = links; //TODO
+    }
 
     return (
-        <div className='flex justify-center pr-2 md:border-r md:border-b-0 border-b mr-4 mb-4 md:mb-0'>
-            <div className='w-full max-w-md flex justify-start md:flex-col flex-row mb-2 md:mb-0'>
-                {links.map((link) => (
+        <div className='flex justify-center pr-2 md:border-r md:border-b-0 border-b md:mr-4 mb-4 md:mb-0'>
+            <div className='w-full max-w-md flex justify-start items-center md:items-start md:flex-col flex-row mb-2 md:mb-0'>
+                {filteredLinks.map((link) => (
                     <Link
                         href={link.to}
                         key={link.to}
-                        className={`px-3 py-2 text-sm font-medium ${pathname === link.to ? 'bg-slate-400 dark:bg-gray-800' : 'text-gray-900 hover:bg-slate-300 dark:text-gray-300 dark:hover:bg-gray-700'}`}>
+                        className={`px-3 py-2 text-sm w-full font-medium ${activeLink === link ? 'bg-slate-400 dark:bg-gray-800' : 'text-gray-900 hover:bg-slate-300 dark:text-gray-300 dark:hover:bg-gray-700'}`}>
                         {link.label}
                     </Link>
                 ))}
