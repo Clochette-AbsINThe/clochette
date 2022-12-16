ddb4e51d4caf1d71a3831ba7a5c925255914e984
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2022-12-16 22:14:46 +0100

    👌 IMPROVE: Refactor saveJwtInCookie

--- frontend/src/pages/api/saveJwtInCookie.ts
+++ frontend/src/pages/api/saveJwtInCookie.ts
@@ -4,12 +4,11 @@ import { parseJwt } from '@utils/utils';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const jwt = req.body['jwt'];
 
-    try {
-        const decodedJWT = parseJwt(jwt);
-        res.setHeader('Set-Cookie', `jwt=${jwt}; Path=/; HttpOnly; SameSite=Lax; Secure; Max-Age=${decodedJWT.exp - decodedJWT.iat};`);
-    } catch {
-        return res.status(400).json({ error: 'Invalid JWT' });
-    }
+    const decodedJWT = parseJwt(jwt);
+
+    if (decodedJWT === null) return res.status(400).json({ error: 'Invalid JWT' });
+
+    res.setHeader('Set-Cookie', `jwt=${jwt}; Path=/; HttpOnly; SameSite=Lax; Secure; Max-Age=${decodedJWT.exp - decodedJWT.iat};`);
     // This redirect is used to prevent the browser from displaying the JSON response and to force the setting of the cookie
     res.redirect(302, '/login');
 }
