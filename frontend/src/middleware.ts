ef56bf106f8118086250e47b1ded939309f278b6
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2022-12-16 22:12:15 +0100

    📦 NEW: Add roles for specific pages in middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,16 +2,24 @@ import { environmentVariable, SECRET_KEY } from '@utils/settings';
 import { jwtVerify } from 'jose';
 import { NextRequest, NextResponse } from 'next/server';
 
+const regex = /\/account\/(?!\/)[^\/]*/;
+
 export async function middleware(req: NextRequest) {
     const jwtCookie = req.cookies.get('jwt')?.value;
 
     if (jwtCookie) {
         try {
             // This function verfies the token with the same secret key as the one used to sign it in the backend
-            const payload = await jwtVerify(jwtCookie, new TextEncoder().encode(SECRET_KEY), { algorithms: ['HS256'] });
+            const jwtResult = await jwtVerify(jwtCookie, new TextEncoder().encode(SECRET_KEY), { algorithms: ['HS256'] });
+
+            // We check the role of the user to see if he has access to the page
+            if (regex.test(req.nextUrl.pathname) && jwtResult.payload?.roles === 'ADMIN') { //TODO
+                return NextResponse.redirect(environmentVariable.BASE_URL + '/account');
+            }
             return NextResponse.next({ status: 200 });
         } catch (error) {
             // If we catch an error here, it means that the token is invalid and we redirect to the login page
+            console.error(error);
         }
     }
     const url = new URL('/login', environmentVariable.BASE_URL);
@@ -19,7 +27,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(url);
 }
 
-// All thoses paths require authentication
+// All thoses paths require authentification
 export const config = {
-    matcher: ['/transaction', '/stock', '/configuration/:path*']
+    matcher: ['/transaction', '/stock', '/configuration/:path*', '/account/:path*']
 };
