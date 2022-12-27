98b220dcf2925127ea02458b424ff31b2b318767
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-12-27 16:05:36 +0100

    Add comments to `base_class` module #33

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -2,17 +2,22 @@ from typing import Any
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
+# Dictionary to store declarative classes
 class_registry: dict = {}
 
 
+# Decorate the `Base` class to make it a declarative base class
 @as_declarative(
     class_registry=class_registry
 )
 class Base:
+    # id field for all models
     id: Any
+    # name of the class
     __name__: str
 
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls) -> str:
+        # Return the lowercase name of the class as the table name
         return cls.__name__.lower()
