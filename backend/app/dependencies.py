f473ccff5aaf77df97f74e8bb1b950e541d15a86
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-12-27 16:17:38 +0100

    Add comments and documentation to `dependencies` module #33

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -17,28 +17,52 @@ translator = Translator()
 
 
 def get_db() -> Generator:
+    """
+    Get a database session.
+
+    :return: A database session
+    """
+    # Create a database session
     db = SessionLocal()
     try:
+        # Yield the session to the calling function
         yield db
     finally:
+        # Close the session after it is no longer needed
         db.close()
 
 
 def get_current_account(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)) -> Account:
+    """
+    Get the current account associated with the JWT token in the authorization header.
+
+    :param db: The database session (dependency injected)
+    :param token: The JWT token in the authorization header (dependency injected)
+    :return: The account associated with the JWT token if the token is valid
+    """
+    # Create an exception to be raised if the token is invalid (i.e. invalid credentials)
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail=translator.AUTHENTICATION_REQUIRED,
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        # Decode the JWT token to get the payload
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM])
+        # Get the username from the payload
         username: str = payload.get("sub")
         if username is None:
+            # Raise an exception if the username is not in the payload
             raise credentials_exception
+        # Create a `TokenData`` object from the username
         token_data = token_schema.TokenData(username=username)
     except JWTError as e:
+        # Raise an exception if the token cannot be decoded
         raise credentials_exception
+    # Get the account associated with the username
     account = (accounts.query(db, limit=1, username=token_data.username)[0:1] or [None])[0]
     if account is None:
+        # Raise an exception if the account does not exist
         raise credentials_exception
+    # Return the account
     return account
