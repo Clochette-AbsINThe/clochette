d3e0e04680301124c12ef53c3b6554877015ab41
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-12-30 22:35:08 +0100

    Add scopes to OAuth2 scheme #91

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -3,4 +3,11 @@ from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f'{settings.API_V1_PREFIX}/auth/login/') # TODO: Shouldn't be hardcoded
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f'{settings.API_V1_PREFIX}/auth/login/', # TODO: Shouldn't be hardcoded
+    scopes={
+        'staff': 'Manage stock and create transactions',
+        'treasurer': 'Staff permissions plus manage treasury, i.e. CRUD over transactions',
+        'president': 'Treasurer permissions plus manage users, i.e. CRUD over users',
+    },
+)
