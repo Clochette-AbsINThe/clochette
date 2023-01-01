a74ef17f8a11f5100cbca6d026d3f5fb039f5c21
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-01-01 15:07:04 +0100

    Require `president` scope to access main account-related endpoints #91

--- backend/app/api/v1/endpoints/account.py
+++ backend/app/api/v1/endpoints/account.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, Security, HTTPException, status
 from typing import Any
 
 from app.core.security import get_password_hash
 from app.core.translation import Translator
 from app.core.utils.misc import to_query_parameters
 from app.crud.crud_account import account as accounts
-from app.dependencies import get_current_account, get_db
+from app.dependencies import get_current_active_account, get_db
 from app.schemas import account as account_schema
 
 
@@ -13,7 +13,7 @@ router = APIRouter(tags=["account"])
 translator = Translator(element="account")
 
 
-@router.get("/", response_model=list[account_schema.Account], dependencies=[Depends(get_current_account)])
+@router.get("/", response_model=list[account_schema.Account], dependencies=[Security(get_current_active_account, scopes=['president'])])
 async def read_accounts(db=Depends(get_db), query=Depends(to_query_parameters(account_schema.AccountBase))) -> list:
     return accounts.query(db, limit=None, **query.dict(exclude_none=True, exclude_unset=True))
 
@@ -28,7 +28,7 @@ async def create_account(account: account_schema.AccountCreate, db=Depends(get_d
     return accounts.create(db, obj_in=account)
 
 
-@router.get("/{account_id}", response_model=account_schema.Account, dependencies=[Depends(get_current_account)])
+@router.get("/{account_id}", response_model=account_schema.Account, dependencies=[Security(get_current_active_account, scopes=['president'])])
 async def read_account(account_id: int, db=Depends(get_db)) -> Any:
     account = accounts.read(db, account_id)
     if account is None:
@@ -39,7 +39,7 @@ async def read_account(account_id: int, db=Depends(get_db)) -> Any:
     return account
 
 
-@router.put("/{account_id}", response_model=account_schema.Account, dependencies=[Depends(get_current_account)])
+@router.put("/{account_id}", response_model=account_schema.Account, dependencies=[Security(get_current_active_account, scopes=['president'])])
 async def update_account(account_id: int, account: account_schema.AccountUpdate, db=Depends(get_db)):
     old_account = accounts.read(db, account_id)
     if old_account is None:
@@ -57,7 +57,7 @@ async def update_account(account_id: int, account: account_schema.AccountUpdate,
     return accounts.update(db, db_obj=old_account, obj_in=account)
 
 
-@router.delete("/{account_id}", response_model=account_schema.Account, dependencies=[Depends(get_current_account)])
+@router.delete("/{account_id}", response_model=account_schema.Account, dependencies=[Security(get_current_active_account, scopes=['president'])])
 async def delete_account(account_id: int, db=Depends(get_db)) -> Any:
     account = accounts.read(db, account_id)
     if account is None:
