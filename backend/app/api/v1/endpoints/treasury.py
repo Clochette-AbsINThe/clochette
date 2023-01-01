c46a344f03a5641afbed26bb835283e6e5514659
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-01-01 15:18:13 +0100

    Require `treasurer` scope to access treasury endpoints #91

--- backend/app/api/v1/endpoints/treasury.py
+++ backend/app/api/v1/endpoints/treasury.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Security, status
 
 from app.core.translation import Translator
 from app.crud.crud_treasury import treasury as treasuries
-from app.dependencies import get_current_account, get_db
+from app.dependencies import get_current_active_account, get_db
 from app.schemas import treasury as treasury_schema
 
 
@@ -10,12 +10,12 @@ router = APIRouter(tags=["treasury"])
 translator = Translator(element="treasury")
 
 
-@router.get("/", response_model=list[treasury_schema.Treasury], dependencies=[Depends(get_current_account)])
+@router.get("/", response_model=list[treasury_schema.Treasury], dependencies=[Security(get_current_active_account, scopes=['treasurer'])])
 async def read_treasuries(db=Depends(get_db)) -> list[treasury_schema.Treasury]:
     return treasuries.read_multi(db)
 
 
-@router.post("/", response_model=treasury_schema.Treasury, dependencies=[Depends(get_current_account)])
+@router.post("/", response_model=treasury_schema.Treasury, dependencies=[Security(get_current_active_account, scopes=['treasurer'])])
 async def create_treasury(treasury: treasury_schema.TreasuryCreate, db=Depends(get_db)) -> dict:
     # If a treasury already exists, raise an error
     if treasuries.read_multi(db):
