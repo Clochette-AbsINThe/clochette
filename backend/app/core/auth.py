8ed6446891d33455aab8ed6cffe38d0aff710593
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-01-01 22:25:09 +0100

    Add method `check_scopes` in `auth` module to check scope validity and hierarchy when authenticating a request #91

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -1,6 +1,7 @@
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 
 from app.core.config import settings
+from app.core.types import SecurityScopes as AccountSecurityScopes
 
 
 oauth2_scheme = OAuth2PasswordBearer(
@@ -11,3 +12,24 @@ oauth2_scheme = OAuth2PasswordBearer(
         'president': 'Manage users, i.e. CRUD over users',
     },
 )
+
+
+def check_scopes(security_scopes: SecurityScopes, token_scopes: list[str]) -> bool:
+    """
+    Check if the token scopes are sufficient to access the endpoint.
+
+    :param security_scopes: The security scopes
+    :param token_scopes: The token scopes
+
+    :return: Whether the token scopes are sufficient to access the endpoint
+    """
+    # Define the scopes hierarchy, as we consider that the president scope is the most powerful, and the staff scope the least powerful
+    scopes_hierarchy = { # TODO: Shouldn't be hardcoded
+        'staff': ['staff'],
+        'treasurer': ['staff', 'treasurer'],
+        'president': ['staff', 'treasurer', 'president'],
+    }
+    # Get the scopes required to access the endpoint
+    required_scopes = security_scopes.scopes
+    # Check if the token scopes are sufficient to access the endpoint
+    return all(scope in scopes_hierarchy[token_scopes[0]] for scope in required_scopes)
