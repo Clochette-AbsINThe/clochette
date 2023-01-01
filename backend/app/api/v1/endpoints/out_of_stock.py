69d793f2e97d8ade9a488ccd3ed0387877110df6
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-01-01 15:14:14 +0100

    Require `staff` scope to access out of stock endpoint #91

--- backend/app/api/v1/endpoints/out_of_stock.py
+++ backend/app/api/v1/endpoints/out_of_stock.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
 
 from app.crud.crud_out_of_stock import out_of_stock as out_of_stocks
-from app.dependencies import get_current_account, get_db
+from app.dependencies import get_current_active_account, get_db
 from app.schemas import out_of_stock as out_of_stock_schema
 
 
 router = APIRouter(tags=["out_of_stock"])
 
 
-@router.get("/", response_model=list[out_of_stock_schema.OutOfStock], response_model_exclude_none=True, dependencies=[Depends(get_current_account)])
+@router.get("/", response_model=list[out_of_stock_schema.OutOfStock], response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
 async def read_out_of_stocks(db=Depends(get_db)) -> list:
     return out_of_stocks.query(db, limit=None)
