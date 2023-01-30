172d7e945e0063246bf1738000f6c43350eb7a6a
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-01-30 17:05:21 +0100

    Create async session using callable class with `AsyncSession` from SQLAlchemy #106

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,23 +1,40 @@
-from sqlalchemy import create_engine
+from abc import ABC, abstractmethod
+from typing import AsyncIterator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
 
-# Create an engine for the database
-engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI,
-    connect_args={
-#        "check_same_thread": False # required for sqlite to work with threads
-    }
-)
+class Database(ABC):
+    def __init__(self):
+        self.async_sessionmaker: sessionmaker | None = None
 
-# Create a session maker to create sessions to interact with the database
-SessionLocal = sessionmaker(
-    # Do not commit changes automatically
-    autocommit=False,
-    # Do not flush changes to the database automatically
-    autoflush=False,
-    # Bind the session maker to the engine
-    bind=engine,
-)
+    async def __call__(self) -> AsyncIterator[AsyncSession]:
+        """
+        Create a new session to interact with the database.
+        To be used by FastAPI's dependency injection system.
+        """
+        if not self.async_sessionmaker:
+            raise ValueError("Database not initialized")
+
+        async with self.async_sessionmaker() as session:
+            yield session
+
+    @abstractmethod
+    def setup(self) -> None:
+        ...
+
+class PostgresDatabase(Database):
+    def setup(self) -> None:
+        """
+        Create a new SQLAlchemy engine and sessionmaker.
+        """
+        async_engine = create_async_engine(
+            settings.SQLALCHEMY_DATABASE_URI,
+        )
+        self.async_sessionmaker = sessionmaker(
+            async_engine,
+            class_=AsyncSession,
+        )
