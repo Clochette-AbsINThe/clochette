a56c4379f4dc6458da70518151f08645a5930ad2
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-02-10 18:03:14 +0100

    Add a date range picker

--- frontend/src/components/Admin/Dashboard/DateRangePicker.tsx
+++ frontend/src/components/Admin/Dashboard/DateRangePicker.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from 'react';
 
 interface DateRangePickerProps {
     startDate: Date;
@@ -10,56 +10,40 @@ export default function DateRangePicker(props: DateRangePickerProps): JSX.Elemen
     const [startDate, setStartDate] = useState<Date>(props.startDate);
     const [endDate, setEndDate] = useState<Date>(props.endDate);
 
-    useEffect(() => {
-        props.setDateRange([startDate, endDate]);
-    }, [startDate, endDate]);
+    const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value !== startDate.toISOString().split('T')[0] && new Date(e.target.value) < endDate && e.target.value !== '') {
+            setStartDate(new Date(e.target.value));
+            props.setDateRange([new Date(e.target.value), endDate]);
+        }
+    };
+
+    const handleEndDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value !== endDate.toISOString().split('T')[0] && new Date(e.target.value) > startDate && e.target.value !== '') {
+            setEndDate(new Date(e.target.value));
+            props.setDateRange([startDate, new Date(e.target.value)]);
+        }
+    };
 
     return (
         <div className='flex items-center'>
-            <div className='relative'>
-                <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
-                    <svg
-                        aria-hidden='true'
-                        className='w-5 h-5 text-gray-500 dark:text-gray-400'
-                        fill='currentColor'
-                        viewBox='0 0 20 20'
-                        xmlns='http://www.w3.org/2000/svg'>
-                        <path
-                            fillRule='evenodd'
-                            d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-                            clipRule='evenodd'></path>
-                    </svg>
-                </div>
+            <div>
                 <input
                     name='start'
                     type='date'
-                    value={startDate.toISOString().split("T")[0]}
-                    onChange={(e) => setStartDate(new Date(e.target.value))}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    value={startDate.toISOString().split('T')[0]}
+                    onChange={handleStartDateChange}
+                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
                     placeholder='Date de début'
                 />
             </div>
             <span className='mx-4 text-gray-500'>à</span>
-            <div className='relative'>
-                <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
-                    <svg
-                        aria-hidden='true'
-                        className='w-5 h-5 text-gray-500 dark:text-gray-400'
-                        fill='currentColor'
-                        viewBox='0 0 20 20'
-                        xmlns='http://www.w3.org/2000/svg'>
-                        <path
-                            fillRule='evenodd'
-                            d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-                            clipRule='evenodd'></path>
-                    </svg>
-                </div>
+            <div>
                 <input
                     name='end'
                     type='date'
-                    value={endDate.toISOString().split("T")[0]}
-                    onChange={(e) => setEndDate(new Date(e.target.value))}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    value={endDate.toISOString().split('T')[0]}
+                    onChange={handleEndDateChange}
+                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
                     placeholder='Date de fin'
                 />
             </div>
