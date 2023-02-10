84ca0d44f5b113b14fc479ce1967f6c3aee2332a
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-02-10 18:03:49 +0100

    Add put frame for profile

--- frontend/src/components/Admin/Dashboard/ProfilePage.tsx
+++ frontend/src/components/Admin/Dashboard/ProfilePage.tsx
@@ -4,6 +4,7 @@ import { Account } from '@types';
 import { useCallback, useEffect, useState } from 'react';
 import AccountForm from './AccountForm';
 
+// TODO: Add modification of account when backend is ready
 export default function ProfilePage() {
     const [account, setAccount] = useState<Account | null>(null);
     const [getAccountData, { loading }] = getPersonnalAccount(setAccount);
@@ -19,11 +20,18 @@ export default function ProfilePage() {
     if (loading || account === null) return <Loader />;
 
     return (
-        <div className='max-h-max max-w-lg'>
-            <AccountForm
-                account={account}
-                editAccount={() => {}}
-            />
-        </div>
+        <>
+            <div className='flex justify-center grow items-center'>
+                <div className='min-w-[50%] border p-4 bg-gray-50 rounded-lg shadow-md dark:bg-gray-800 flex flex-col justify-around dark:border-gray-500'>
+                    <div className='flex flex-col items-center'>
+                        <h1 className='text-2xl font-bold md:mb-12 mb-6'>Modification de votre compte</h1>
+                    </div>
+                    <AccountForm
+                        account={account}
+                        editAccount={(account) => console.log(account)}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
