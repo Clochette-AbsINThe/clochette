74a38db67b6e51fbfb6ff78d274264ffe55e16fd
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-03-04 23:02:24 +0100

    `items` fiels can be `None` only if transaction type is `tresorery` #87

--- backend/app/schemas/transaction.py
+++ backend/app/schemas/transaction.py
@@ -1,5 +1,7 @@
 import datetime
 
+from pydantic import validator
+
 from app.core.config import DefaultModel
 from app.core.types import PaymentMethod, TransactionType
 from app.schemas.barrel import Barrel
@@ -24,7 +26,12 @@ class TransactionCreate(TransactionBase):
 
 class TransactionFrontCreate(TransactionBase):
     amount: float
-    items: list[Item]
+    items: list[Item] | None
+
+    @validator('items')
+    def none_only_if_tresorery_type(cls, v, values):
+        if v is None and values['type'] != TransactionType.tresorery:
+            raise ValueError('items must be provided if type is not "tresorery"')
 
 
 class TransactionUpdate(TransactionBase):
