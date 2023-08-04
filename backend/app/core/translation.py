dd86a57a22812b86e8183289d923f54e372817f3
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-08-04 16:14:47 +0200

    🐛 Fix translated fromat elements

--- backend/app/core/translation.py
+++ backend/app/core/translation.py
@@ -1,93 +1,102 @@
 from dataclasses import dataclass
 
-from app.core.config import settings
+from app.core.config import SupportedLocales, settings
 
-
-SUPPORTED_LOCALES = ["en", "fr"]
+ElementNameMapper: dict[SupportedLocales, str] = {"en": "element", "fr": "élément"}
 
 
 @dataclass
 class Translator:
-    """ Class to manage the translation of the messages. """
+    """Class to manage the translation of the messages."""
 
     class TranslatedString:
-        """ Class to manage the translation of a single string. """
-        def __init__(self, strings: dict[str, str]):
+        """Class to manage the translation of a single string."""
+
+        def __init__(self, strings: dict[SupportedLocales, str]):
             # Check if locales are supported
-            for locale in strings:
-                if locale not in SUPPORTED_LOCALES:
-                    raise ValueError(f"Locale '{locale}' is not supported.")
-            self._strings: dict[str, str] = strings
+            self._strings: dict[SupportedLocales, str] = strings
             self._element: str | None = None
 
         def __str__(self) -> str:
-            """ Return the string corresponding to the current locale. """
-            if self._element is None:
-                return self._strings[settings.LOCALE].capitalize()
-            return self._strings[settings.LOCALE].format(element=self._element).capitalize()
-    
+            """Return the string corresponding to the current locale."""
+            element = (
+                self._element
+                if self._element is not None
+                else ElementNameMapper[settings.LOCALE]
+            )
+            return (
+                self._strings[settings.LOCALE]
+                .format(element=element)
+                .replace("_", " ")
+                .capitalize()
+            )
+
     def __init__(self, element: str | None = None):
         super().__init__()
         self._element = element
-    
+
     # Define the messages here
-    ELEMENT_NOT_FOUND: TranslatedString = TranslatedString({
-        "en": "Element not found" if format is None else "Element {element} not found",
-        "fr": "Élément introuvable" if format is None else "Élément {element} introuvable"
-    })
+    ELEMENT_NOT_FOUND: TranslatedString = TranslatedString(
+        strings={
+            "en": "{element} not found",
+            "fr": "{element} introuvable",
+        }
+    )
 
-    ELEMENT_ALREADY_EXISTS: TranslatedString = TranslatedString({
-        "en": "Element already exists" if format is None else "Element {element} already exists",
-        "fr": "Élément déjà existant" if format is None else "Élément {element} déjà existant"
-    })
+    ELEMENT_ALREADY_EXISTS: TranslatedString = TranslatedString(
+        {
+            "en": "{element} already exists",
+            "fr": "{element} déjà existant",
+        }
+    )
 
-    INVALID_CREDENTIALS: TranslatedString = TranslatedString({
-        "en": "Invalid credentials",
-        "fr": "Identifiants invalides"
-    })
+    INVALID_CREDENTIALS: TranslatedString = TranslatedString(
+        {"en": "Invalid credentials", "fr": "Identifiants invalides"}
+    )
 
-    INTEGRITY_ERROR: TranslatedString = TranslatedString({
-        "en": "Data relationship integrity error",
-        "fr": "Erreur d'intégrité des données"
-    })
+    INTEGRITY_ERROR: TranslatedString = TranslatedString(
+        {
+            "en": "Data relationship integrity error",
+            "fr": "Erreur d'intégrité des données",
+        }
+    )
 
-    DELETION_OF_USED_ELEMENT: TranslatedString = TranslatedString({
-        "en": "Element is in use and cannot be deleted" if format is None else "Element {element} is in use and cannot be deleted",
-        "fr": "L'élément est utilisé et ne peut pas être supprimé" if format is None else "L'élément {element} est utilisé et ne peut pas être supprimé"
-    })
+    DELETION_OF_USED_ELEMENT: TranslatedString = TranslatedString(
+        {
+            "en": "{element} is in use and cannot be deleted",
+            "fr": "{element} est utilisé et ne peut pas être supprimé",
+        }
+    )
 
-    NEGATIVE_CASH_AMOUNT: TranslatedString = TranslatedString({
-        "en": "Cash amount cannot be negative",
-        "fr": "Le montant en espèces ne peut pas être négatif"
-    })
+    NEGATIVE_CASH_AMOUNT: TranslatedString = TranslatedString(
+        {
+            "en": "Cash amount cannot be negative",
+            "fr": "Le montant en espèces ne peut pas être négatif",
+        }
+    )
 
-    INTERNAL_SERVER_ERROR: TranslatedString = TranslatedString({
-        "en": "Internal server error",
-        "fr": "Erreur interne du serveur"
-    })
+    INTERNAL_SERVER_ERROR: TranslatedString = TranslatedString(
+        {"en": "Internal server error", "fr": "Erreur interne du serveur"}
+    )
 
-    AUTHENTICATION_REQUIRED: TranslatedString = TranslatedString({
-        "en": "Authentication required",
-        "fr": "Authentification requise"
-    })
+    AUTHENTICATION_REQUIRED: TranslatedString = TranslatedString(
+        {"en": "Authentication required", "fr": "Authentification requise"}
+    )
 
-    INSUFFICIENT_PERMISSIONS: TranslatedString = TranslatedString({
-        "en": "Insufficient permissions",
-        "fr": "Permissions insuffisantes"
-    })
+    INSUFFICIENT_PERMISSIONS: TranslatedString = TranslatedString(
+        {"en": "Insufficient permissions", "fr": "Permissions insuffisantes"}
+    )
 
-    INACTIVE_ACCOUNT: TranslatedString = TranslatedString({
-        "en": "Account is inactive",
-        "fr": "Le compte est inactif"
-    })
+    INACTIVE_ACCOUNT: TranslatedString = TranslatedString(
+        {"en": "Account is inactive", "fr": "Le compte est inactif"}
+    )
 
-    USERNAME_UNAVAILABLE: TranslatedString = TranslatedString({
-        "en": "Username is unavailable",
-        "fr": "Le nom d'utilisateur est indisponible"
-    })
+    USERNAME_UNAVAILABLE: TranslatedString = TranslatedString(
+        {"en": "Username is unavailable", "fr": "Le nom d'utilisateur est indisponible"}
+    )
 
     def __getattribute__(self, __name: str) -> str:
-        """ Get the translated string. """
+        """Get the translated string."""
         attr = super().__getattribute__(__name)
         if isinstance(attr, Translator.TranslatedString):
             attr._element = self._element
