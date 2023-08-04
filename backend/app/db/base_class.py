121cfe3abbb36e5093ee4592b39951685b2968f5
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-08-04 16:36:20 +0200

    🎨 Add a method to create SQLAlchemy model for test

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,15 +1,16 @@
-from typing import Any
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from typing import TYPE_CHECKING, Any, Type, TypeVar
 
+from annotated_types import T
+from sqlalchemy.orm import as_declarative, declared_attr
 
 # Dictionary to store declarative classes
 class_registry: dict = {}
 
+_T = TypeVar("_T", bound="Base")
+
 
 # Decorate the `Base` class to make it a declarative base class
-@as_declarative(
-    class_registry=class_registry
-)
+@as_declarative(class_registry=class_registry)
 class Base:
     # id field for all models
     id: Any
@@ -17,7 +18,37 @@ class Base:
     __name__: str
 
     # Generate __tablename__ automatically
-    @declared_attr
+    @declared_attr.directive
     def __tablename__(cls) -> str:
-        # Return the lowercase name of the class as the table name
         return cls.__name__.lower()
+
+    @classmethod
+    def create(cls: Type[_T], **kwargs: Any) -> _T:
+        """
+        Create a new instance of the class and set its attributes based on the provided keyword arguments.
+        This is used in the tests to create new instances of the model classes.
+
+        :param kwargs: The keyword arguments to set as attributes on the new instance.
+        :return: The new instance of the class.
+        :raises ValueError: If a provided keyword argument is not a valid attribute for the class.
+        """
+        instance = cls()
+        for key, value in kwargs.items():
+            if hasattr(instance, key):
+                setattr(instance, key, value)
+            else:
+                raise ValueError(
+                    f"{key} is not a valid attribute for {instance.__class__.__name__}"
+                )
+        return instance
+
+
+if TYPE_CHECKING:
+    from sqlalchemy.sql.type_api import TypeEngine
+
+    class Enum(TypeEngine[T]):
+        def __init__(self, enum: Type[T]) -> None:
+            ...
+
+else:
+    from sqlalchemy import Enum  # noqa: F401
