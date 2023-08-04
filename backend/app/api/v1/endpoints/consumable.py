621102a030840efdd62d994770103c17b36a3e84
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-08-04 16:33:50 +0200

    📝 🔊 Add logger and documentations to endpoints v1

--- backend/app/api/v1/endpoints/consumable.py
+++ backend/app/api/v1/endpoints/consumable.py
@@ -1,30 +1,90 @@
+import logging
+
 from fastapi import APIRouter, Depends, HTTPException, Security, status
 
 from app.core.translation import Translator
 from app.crud.crud_consumable import consumable as consumables
 from app.dependencies import get_current_active_account, get_db
+from app.models import consumable as consumable_model
 from app.schemas import consumable as consumable_schema
 
-
-router = APIRouter(tags=["consumable"])
+router = APIRouter(tags=["consumable"], prefix="/consumable")
 translator = Translator(element="consumable")
 
-
-@router.get("/", response_model=list[consumable_schema.Consumable], dependencies=[Security(get_current_active_account)])
-async def read_consumables(db=Depends(get_db), all: bool = False) -> list:
-    return await consumables.query(db, limit=None) if all else await consumables.query(db, empty=False, limit=None)
+logger = logging.getLogger("app.api.v1.consumable")
 
 
-@router.get("/distincts/", response_model=list[consumable_schema.Consumable], dependencies=[Security(get_current_active_account)])
-async def read_consumables_distincts(db=Depends(get_db)) -> list:
-    return await consumables.query(db, distinct='consumable_item_id', empty=False, limit=None)
+@router.get(
+    "/",
+    response_model=list[consumable_schema.Consumable],
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_consumables(db=Depends(get_db), all: bool = False):
+    """
+    Retrieve a list of consumables.
 
-@router.put("/{consumable_id}", response_model=consumable_schema.Consumable, dependencies=[Security(get_current_active_account)])
-async def update_consumable(consumable_id: int, consumable: consumable_schema.ConsumableUpdate, db=Depends(get_db)) -> consumable_schema.Consumable:
+    Args:
+        db: The database connection dependency.
+        all: A boolean indicating whether to return all consumables or only non-empty ones.
+
+    Returns:
+        A list of consumables.
+    """
+    logger.debug(f"all: {all}")
+    if all:
+        return await consumables.query(db, limit=None)
+
+    return await consumables.query(db, empty=False, limit=None)
+
+
+@router.get(
+    "/distincts/",
+    response_model=list[consumable_schema.Consumable],
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_consumables_distincts(db=Depends(get_db)):
+    """
+    Retrieve a list of distinct consumables.
+
+    Args:
+        db: The database connection dependency.
+
+    Returns:
+        A list of distinct consumables.
+    """
+    return await consumables.query(
+        db,
+        distinct=consumable_model.Consumable.consumable_item_id,
+        empty=False,
+        limit=None,
+    )
+
+
+@router.put(
+    "/{consumable_id}",
+    response_model=consumable_schema.Consumable,
+    dependencies=[Security(get_current_active_account)],
+)
+async def update_consumable(
+    consumable_id: int,
+    consumable: consumable_schema.ConsumableUpdate,
+    db=Depends(get_db),
+):
+    """
+    Update a consumable.
+
+    Args:
+        consumable_id: The ID of the consumable to update.
+        consumable: The updated consumable data.
+        db: The database connection dependency.
+
+    Returns:
+        The updated consumable.
+    """
     db_consumable = await consumables.read(db, consumable_id)
     if db_consumable is None:
+        logger.debug(f"Consumable {consumable_id} not found")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=translator.ELEMENT_NOT_FOUND
+            status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND
         )
     return await consumables.update(db, db_obj=db_consumable, obj_in=consumable)
--- backend/app/api/v1/endpoints/out_of_stock_item.py
+++ backend/app/api/v1/endpoints/out_of_stock_item.py
@@ -1,77 +1,217 @@
+import logging
+
 from fastapi import APIRouter, Depends, HTTPException, Security, status
 
 from app.core.translation import Translator
 from app.core.utils.misc import process_query_parameters, to_query_parameters
-from app.crud.crud_out_of_stock_item import out_of_stock_item as out_of_stock_items
 from app.crud.crud_out_of_stock import out_of_stock as out_of_stocks
+from app.crud.crud_out_of_stock_item import out_of_stock_item as out_of_stock_items
 from app.dependencies import get_current_active_account, get_db
 from app.schemas import out_of_stock_item as out_of_stock_item_schema
 
-
-router = APIRouter(tags=["out_of_stock_item"])
+router = APIRouter(tags=["out_of_stock_item"], prefix="/out_of_stock_item")
 translator = Translator(element="out_of_stock_item")
 
-
-@router.get("/buy/", response_model=list[out_of_stock_item_schema.OutOfStockItem], response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
-async def read_out_of_stock_items_buy(db=Depends(get_db), query=Depends(to_query_parameters(out_of_stock_item_schema.OutOfStockItemBase))) -> list:
-    return await out_of_stock_items.query(db, limit=None, buy_or_sell=True, **process_query_parameters(query))
+logger = logging.getLogger("app.api.v1.out_of_stock_item")
 
 
-@router.get("/sell/", response_model=list[out_of_stock_item_schema.OutOfStockItem], response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
-async def read_out_of_stock_items_sell(db=Depends(get_db), query=Depends(to_query_parameters(out_of_stock_item_schema.OutOfStockItemBase))) -> list:
-    return await out_of_stock_items.query(db, limit=None, buy_or_sell=False, **process_query_parameters(query))
+@router.get(
+    "/buy/",
+    response_model=list[out_of_stock_item_schema.OutOfStockItem],
+    response_model_exclude_none=True,
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_out_of_stock_items_buy(
+    db=Depends(get_db),
+    query=Depends(to_query_parameters(out_of_stock_item_schema.OutOfStockItemBase)),
+):
+    """
+    Retrieve a list of out of stock items for buying.
+
+    Args:
+        db: the database connection dependency.
+        query: the query parameters to filter the results.
+
+    Returns:
+        A list of out of stock items for buying.
+    """
+    query_parameters = process_query_parameters(query)
+    logger.debug(f"Query parameters: {query_parameters}")
+    return await out_of_stock_items.query(
+        db, limit=None, buy_or_sell=True, **query_parameters
+    )
 
 
-@router.get("/{out_of_stock_item_id}", response_model=out_of_stock_item_schema.OutOfStockItem, response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
-async def read_out_of_stock_item(out_of_stock_item_id: int, db=Depends(get_db)) -> dict:
+@router.get(
+    "/sell/",
+    response_model=list[out_of_stock_item_schema.OutOfStockItem],
+    response_model_exclude_none=True,
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_out_of_stock_items_sell(
+    db=Depends(get_db),
+    query=Depends(to_query_parameters(out_of_stock_item_schema.OutOfStockItemBase)),
+):
+    """
+    Retrieve a list of out of stock items for selling.
+
+    Args:
+        db: the database connection dependency.
+        query: the query parameters to filter the results.
+
+    Returns:
+        A list of out of stock items for selling.
+    """
+    query_parameters = process_query_parameters(query)
+    logger.debug(f"Query parameters: {query_parameters}")
+    return await out_of_stock_items.query(
+        db, limit=None, buy_or_sell=False, **query_parameters
+    )
+
+
+@router.get(
+    "/{out_of_stock_item_id}",
+    response_model=out_of_stock_item_schema.OutOfStockItem,
+    response_model_exclude_none=True,
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_out_of_stock_item(out_of_stock_item_id: int, db=Depends(get_db)):
+    """
+    Retrieve an out of stock item by ID.
+
+    Args:
+        out_of_stock_item_id: the ID of the out of stock item to retrieve.
+        db: the database connection dependency.
+
+    Returns:
+        The out of stock item with the specified ID.
+
+    Raises:
+        HTTPException: if the out of stock item is not found.
+    """
     out_of_stock_item = await out_of_stock_items.read(db, out_of_stock_item_id)
     if out_of_stock_item is None:
+        logger.debug(f"Out of stock item {out_of_stock_item_id} not found")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=translator.ELEMENT_NOT_FOUND
+            status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND
         )
     return out_of_stock_item
 
 
-@router.post("/", response_model=out_of_stock_item_schema.OutOfStockItem, response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
-async def create_out_of_stock_item(out_of_stock_item: out_of_stock_item_schema.OutOfStockItemCreateFront, db=Depends(get_db)) -> dict:
-    if await out_of_stock_items.query(db, limit=1, name=out_of_stock_item.name, buy_or_sell=out_of_stock_item.sell_price is None):
+@router.post(
+    "/",
+    response_model=out_of_stock_item_schema.OutOfStockItem,
+    response_model_exclude_none=True,
+    dependencies=[Security(get_current_active_account)],
+)
+async def create_out_of_stock_item(
+    out_of_stock_item: out_of_stock_item_schema.OutOfStockItemCreate,
+    db=Depends(get_db),
+):
+    """
+    Create a new out of stock item.
+
+    Args:
+        out_of_stock_item: the data for the new out of stock item.
+        db: the database connection dependency.
+
+    Returns:
+        The newly created out of stock item.
+
+    Raises:
+        HTTPException: if an out of stock item with the same name already exists.
+    """
+    if await out_of_stock_items.query(
+        db,
+        limit=1,
+        name=out_of_stock_item.name,
+        buy_or_sell=out_of_stock_item.sell_price is None,
+    ):
+        logger.debug(f"Out of stock item {out_of_stock_item.name} already exists")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=translator.ELEMENT_ALREADY_EXISTS
+            detail=translator.ELEMENT_ALREADY_EXISTS,
         )
-    saved_model = out_of_stock_item_schema.OutOfStockItemCreate(**out_of_stock_item.dict(), buy_or_sell=out_of_stock_item.sell_price is None)
-    return await out_of_stock_items.create(db, obj_in=saved_model.dict(by_alias=False))
+    return await out_of_stock_items.create(db, obj_in=out_of_stock_item)
 
 
-@router.put("/{out_of_stock_item_id}", response_model=out_of_stock_item_schema.OutOfStockItem, response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
-async def update_out_of_stock_item(out_of_stock_item_id: int, out_of_stock_item: out_of_stock_item_schema.OutOfStockItemUpdateFront, db=Depends(get_db)):
+@router.put(
+    "/{out_of_stock_item_id}",
+    response_model=out_of_stock_item_schema.OutOfStockItem,
+    response_model_exclude_none=True,
+    dependencies=[Security(get_current_active_account)],
+)
+async def update_out_of_stock_item(
+    out_of_stock_item_id: int,
+    out_of_stock_item: out_of_stock_item_schema.OutOfStockItemUpdate,
+    db=Depends(get_db),
+):
+    """
+    Update an existing out of stock item.
+
+    Args:
+        out_of_stock_item_id: the ID of the out of stock item to update.
+        out_of_stock_item: the updated data for the out of stock item.
+        db: the database connection dependency.
+
+    Returns:
+        The updated out of stock item.
+
+    Raises:
+        HTTPException: if the out of stock item is not found or an out of stock item with the same name already exists.
+    """
     old_out_of_stock_item = await out_of_stock_items.read(db, out_of_stock_item_id)
     if old_out_of_stock_item is None:
+        logger.debug(f"Out of stock item {out_of_stock_item_id} not found")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=translator.ELEMENT_NOT_FOUND
+            status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND
         )
-    results = await out_of_stock_items.query(db, limit=1, name=out_of_stock_item.name, buy_or_sell=out_of_stock_item.sell_price is None)
+    results = await out_of_stock_items.query(
+        db,
+        limit=1,
+        name=out_of_stock_item.name,
+        buy_or_sell=out_of_stock_item.sell_price is None,
+    )
     if results and results[0].id != old_out_of_stock_item.id:
+        logger.debug(f"Out of stock item {out_of_stock_item.name} already exists")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=translator.ELEMENT_ALREADY_EXISTS
+            detail=translator.ELEMENT_ALREADY_EXISTS,
         )
-    saved_model = out_of_stock_item_schema.OutOfStockItemUpdate(**out_of_stock_item.dict(), buy_or_sell=out_of_stock_item.sell_price is None)
-    return await out_of_stock_items.update(db, db_obj=old_out_of_stock_item, obj_in=saved_model)
+    return await out_of_stock_items.update(
+        db, db_obj=old_out_of_stock_item, obj_in=out_of_stock_item
+    )
 
 
-@router.delete("/{out_of_stock_item_id}", response_model=out_of_stock_item_schema.OutOfStockItem, response_model_exclude_none=True, dependencies=[Security(get_current_active_account)])
+@router.delete(
+    "/{out_of_stock_item_id}",
+    response_model=out_of_stock_item_schema.OutOfStockItem,
+    response_model_exclude_none=True,
+    dependencies=[Security(get_current_active_account)],
+)
 async def delete_out_of_stock_item(out_of_stock_item_id: int, db=Depends(get_db)):
+    """
+    Delete an existing out of stock item.
+
+    Args:
+        out_of_stock_item_id: the ID of the out of stock item to delete.
+        db: the database connection dependency.
+
+    Returns:
+        The deleted out of stock item.
+
+    Raises:
+        HTTPException: if the out of stock item is not found or is used in an out of stock.
+    """
     if await out_of_stock_items.read(db, out_of_stock_item_id) is None:
+        logger.debug(f"Out of stock item {out_of_stock_item_id} not found")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=translator.ELEMENT_NOT_FOUND
+            status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND
         )
     if await out_of_stocks.query(db, limit=1, item_id=out_of_stock_item_id):
+        logger.debug(f"Out of stock item {out_of_stock_item_id} is used")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=translator.DELETION_OF_USED_ELEMENT
+            detail=translator.DELETION_OF_USED_ELEMENT,
         )
     return await out_of_stock_items.delete(db, id=out_of_stock_item_id)
