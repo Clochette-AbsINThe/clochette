1cba61008efdc124f3e6b612ea3137f52d395af1
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-08-04 16:17:13 +0200

    🎨 Use of model_dump to get data in CRUD

--- backend/app/crud/base.py
+++ backend/app/crud/base.py
@@ -1,37 +1,35 @@
 from datetime import datetime
-from typing import Any, Generic, Optional, Type, TypeVar
+from typing import Any, Generic, Optional, Sequence, Type, TypeVar
 
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+from sqlalchemy import ColumnElement
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql.expression import select, Select
+from sqlalchemy.sql.expression import Select, select
 
 from app.core.decorator import handle_exceptions
 from app.core.translation import Translator
-from app.core.types import SynchronizedClass
 from app.db.base_class import Base
-
+from app.schemas.base import DefaultModel
 
 translator = Translator()
 
 # SQLAlchemy model representing the object
-ModelType = TypeVar("ModelType", bound=Base)
+ModelT = TypeVar("ModelT", bound=Base)
 # Pydantic validation schema for creating the object
-CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+CreateSchemaT = TypeVar("CreateSchemaT", bound=DefaultModel)
 # Pydantic validation schema for updating the object
-UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+UpdateSchemaT = TypeVar("UpdateSchemaT", bound=DefaultModel)
 
 
 class CRUDBase(
     Generic[
-        ModelType,
-        CreateSchemaType,
-        UpdateSchemaType,
-    ],
-    SynchronizedClass
+        ModelT,
+        CreateSchemaT,
+        UpdateSchemaT,
+    ]
 ):
-    def __init__(self, model: Type[ModelType]):
+    def __init__(self, model: Type[ModelT]):
         """
         CRUD object with default methods to Create, Read, Update and Delete.
 
@@ -40,7 +38,9 @@ class CRUDBase(
         super().__init__()
         self.model = model
 
-    async def read(self, db: AsyncSession, id: Any, for_update: bool = False) -> Optional[ModelType]:
+    async def read(
+        self, db: AsyncSession, id: Any, for_update: bool = False
+    ) -> Optional[ModelT]:
         """
         Get a record by id.
 
@@ -52,7 +52,14 @@ class CRUDBase(
         """
         return await db.get(self.model, id, with_for_update=for_update)
 
-    async def query(self, db: AsyncSession, distinct: str | None = None, skip: int = 0, limit: int = 100, **filters) -> list[ModelType]:
+    async def query(
+        self,
+        db: AsyncSession,
+        distinct: ColumnElement[Any] | None = None,
+        skip: int = 0,
+        limit: int | None = 100,
+        **filters
+    ) -> Sequence[ModelT]:
         """
         Get multiple records with filters and distinct option.
 
@@ -75,25 +82,31 @@ class CRUDBase(
         query: Select = select(self.model)
 
         for column, value in filters.items():
-            if isinstance(value, dict):  # Check whether the value of the filter is a dictionary
-                for operator, operand in value.items():  # Iterate over the items in the value dictionary, which should contain operator-operand pairs
+            if isinstance(
+                value, dict
+            ):  # Check whether the value of the filter is a dictionary
+                for (
+                    operator,
+                    operand,
+                ) in (
+                    value.items()
+                ):  # Iterate over the items in the value dictionary, which should contain operator-operand pairs
                     # The operator variable is used to specify the operator to use in the filter
                     # (e.g. > and <), and the operand variable is used as the operand for
                     # the filter. For example, if the value dictionary contained the items {gt: 10},
                     # (the operator comes from the operator module) the filter applied would be column > 10.
-                    query = query.where(
-                        operator(getattr(self.model, column), operand))
+                    query = query.where(operator(getattr(self.model, column), operand))
             else:
                 query = query.where(getattr(self.model, column) == value)
 
-        if distinct:
+        if distinct is not None:
             # Apply the DISTINCT option if specified
-            query = query.distinct(distinct)
+            query = query.distinct(distinct).group_by(distinct)
 
         return (await db.execute(query.offset(skip).limit(limit))).scalars().all()
 
     @handle_exceptions(translator.INTEGRITY_ERROR, IntegrityError)
-    async def create(self, db: AsyncSession, *, obj_in: CreateSchemaType) -> ModelType:
+    async def create(self, db: AsyncSession, *, obj_in: CreateSchemaT) -> ModelT:
         """
         Create a new record.
 
@@ -103,25 +116,28 @@ class CRUDBase(
         :return: The created record
         """
         # The jsonable_encoder function is used to convert the Pydantic schema to a dictionary
-        obj_in_data = jsonable_encoder(
-            obj_in,
-            by_alias=False,  # by_alias=False means that the keys of the dictionary will be the same as the field names in the Pydantic schema in order to match the column names in the database
-            custom_encoder={
-                # Do not convert datetime objects to strings
-                datetime: lambda v: v,
-            }
-        )
+        # by_alias=False means that the keys of the dictionary will be the same as the field names in
+        # the Pydantic schema in order to match the column names in the database
+        obj_in_data = obj_in.model_dump(by_alias=False)
         # Create a new model instance from the input data
         db_obj = self.model(**obj_in_data)
         # Add the new model instance to the database session
         db.add(db_obj)
         # Commit the session to persist the model instance in the database
         await db.commit()
+        # Refresh the model instance to get the default values for the columns
+        await db.refresh(db_obj)
         # Return the created model instance
         return db_obj
 
     @handle_exceptions(translator.INTEGRITY_ERROR, IntegrityError)
-    async def update(self, db: AsyncSession, *, db_obj: ModelType, obj_in: UpdateSchemaType | dict[str, Any]) -> ModelType:
+    async def update(
+        self,
+        db: AsyncSession,
+        *,
+        db_obj: ModelT,
+        obj_in: UpdateSchemaT | dict[str, Any]
+    ) -> ModelT:
         """
         Update a record.
 
@@ -137,7 +153,7 @@ class CRUDBase(
             custom_encoder={
                 # Do not convert datetime objects to strings
                 datetime: lambda v: v,
-            }
+            },
         )
         # If the input data is a dictionary, use it as the update data
         # otherwise encode the input data as a dictionary to get the update data
@@ -145,7 +161,7 @@ class CRUDBase(
             update_data = obj_in
         else:
             # exclude_unset=True means that only the fields that are explicitly set in the input data will be updated
-            update_data = obj_in.dict(exclude_unset=True)
+            update_data = obj_in.model_dump(exclude_unset=True, exclude_defaults=True)
 
         # Update the fields of the database object with the corresponding
         # values from the update data
@@ -156,9 +172,10 @@ class CRUDBase(
                 setattr(db_obj, field, update_data[field])
         db.add(db_obj)
         await db.commit()
+        await db.refresh(db_obj)
         return db_obj
 
-    async def delete(self, db: AsyncSession, *, id: int) -> ModelType:
+    async def delete(self, db: AsyncSession, *, id: int) -> ModelT | None:
         """
         Delete a record.
 
