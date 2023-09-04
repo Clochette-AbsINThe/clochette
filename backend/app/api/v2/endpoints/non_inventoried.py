0db91ec00f186835555c659ae92d0bb7e3f7e654
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-09-04 16:18:31 +0200

    ✨ Add new non inventoried endpoint

---
+++ backend/app/api/v2/endpoints/non_inventoried.py
@@ -0,0 +1,76 @@
+import logging
+
+from fastapi import APIRouter, Depends, HTTPException, Security, status
+
+from app.core.translation import Translator
+from app.crud.crud_non_inventoried import non_inventoried as non_inventorieds
+from app.dependencies import get_current_active_account, get_db
+from app.schemas.v2 import non_inventoried as non_inventoried_schema
+
+router = APIRouter(tags=["non_inventoried"], prefix="/non_inventoried")
+
+translator = Translator(element="non_inventoried")
+logger = logging.getLogger("app.api.v2.endpoints.non_inventoried")
+
+
+@router.get(
+    "/",
+    response_model=list[non_inventoried_schema.NonInventoried],
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_non_inventorieds(db=Depends(get_db)):
+    """
+    Retrieve a list of non inventoried items.
+
+    Returns:
+        list[non_inventoried_schema.NonInventoried]: A list of non inventoried items.
+    """
+    return await non_inventorieds.query(db, limit=None)
+
+
+@router.get(
+    "/{non_inventoried_id}",
+    response_model=non_inventoried_schema.NonInventoried,
+    dependencies=[Security(get_current_active_account)],
+)
+async def read_non_inventoried(non_inventoried_id: int, db=Depends(get_db)):
+    """
+    Retrieve a non inventoried item.
+
+    Args:
+        non_inventoried_id (int): Non inventoried item id.
+
+    Returns:
+        non_inventoried_schema.NonInventoried: A non inventoried item.
+
+    Raises:
+        HTTPException: If the non inventoried item is not found.
+    """
+    non_inventoried = await non_inventorieds.read(db, id=non_inventoried_id)
+    if not non_inventoried:
+        logger.debug(f"NonInventoried {non_inventoried_id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=translator.ELEMENT_NOT_FOUND,
+        )
+    return non_inventoried
+
+
+@router.post(
+    "/",
+    response_model=non_inventoried_schema.NonInventoried,
+    dependencies=[Security(get_current_active_account)],
+)
+async def create_non_inventoried(
+    non_inventoried: non_inventoried_schema.NonInventoriedCreate, db=Depends(get_db)
+):
+    """
+    Create a new non inventoried item.
+
+    Args:
+        non_inventoried (non_inventoried_schema.NonInventoriedCreate): Non inventoried item to create.
+
+    Returns:
+        non_inventoried_schema.NonInventoried: The created non inventoried item.
+    """
+    return await non_inventorieds.create_v2(db, obj_in=non_inventoried)
---
+++ backend/app/schemas/v2/non_inventoried.py
@@ -0,0 +1,51 @@
+from pydantic import ConfigDict, Field, PrivateAttr, computed_field
+
+from app.core.types import IconName
+from app.schemas.base import DefaultModel, ExcludedField
+from app.schemas.v2.non_inventoried_item import NonInventoriedItem
+
+
+class NonInventoriedBase(DefaultModel):
+    non_inventoried_item_id: int
+    buy_price: float | None = Field(default=None, gt=0)
+
+
+class NonInventoriedCreate(NonInventoriedBase):
+    transaction_id: int
+
+    _sell_price: float | None = PrivateAttr(default=None)
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def sell_price(self) -> float | None:
+        return self._sell_price
+
+    @sell_price.setter
+    def sell_price(self, value: float | None) -> None:
+        self._sell_price = value
+
+
+class NonInventoriedUpdate(NonInventoriedBase):
+    pass
+
+
+class NonInventoried(NonInventoriedBase):
+    id: int
+    non_inventoried_item: NonInventoriedItem | None = ExcludedField
+    sell_price: float | None
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def name(self) -> str:
+        return self.non_inventoried_item.name if self.non_inventoried_item else "N/A"
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def icon(self) -> IconName:
+        return (
+            self.non_inventoried_item.icon
+            if self.non_inventoried_item
+            else IconName.MISC
+        )
+
+    model_config = ConfigDict(from_attributes=True)
