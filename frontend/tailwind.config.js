aa8362d1a548fd5f57c40f7edd35d644740e14e5
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-04 13:50:46 +0200

    ➕ Add shadcn/ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,87 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            keyframes: {
-                'slide-in': {
-                    '0%': { transform: 'translateY(-10%)', opacity: 0 },
-                    '100%': { transform: 'translateX(0)', opacity: 1 }
-                },
-                'fade-in': {
-                    '0%': { opacity: 0 },
-                    '100%': { opacity: 1 }
-                }
-            },
-            animation: {
-                'slide-in': 'slide-in 0.9s forwards 0.8s',
-                'fade-in': 'fade-in 0.5s forwards'
-            }
-        }
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    safelist: ['h-24', 'w-24', 'text-gray-500', 'dark:text-gray-100'],
-    plugins: [],
-    darkMode: 'class'
+    extend: {
+      colors: {
+        "semi-transparent": "rgba(112, 112, 112, 0.09)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        'slide-in': {
+          '0%': { transform: 'translateY(-10%)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 }
+        },
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        'slide-in': 'slide-in 0.9s forwards 0.8s',
+        'fade-in': 'fade-in 0.5s forwards'
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 };
