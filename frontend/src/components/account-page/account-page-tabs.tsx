8141f6dd44072923697132d6e13820041b91d5c5
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-04 14:24:14 +0200

    ✨ Add `account-page` components

---
+++ frontend/src/components/account-page/account-page-tabs.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+import { SecurityScopes } from '@/openapi-codegen/clochetteSchemas';
+import { pages } from '@/utils/pages';
+
+type LinksType = Array<{
+  label: string;
+  href: string;
+  scopes: SecurityScopes[];
+}>;
+
+export const links: LinksType = [
+  { label: 'Accueil', href: pages.account.index, scopes: ['staff', 'treasurer', 'president'] },
+  { label: 'Tableau de bord', href: pages.account.dashboard, scopes: ['president', 'treasurer'] },
+  { label: 'Comptes utilisateurs', href: pages.account.users, scopes: ['president'] },
+  { label: 'Trésorie', href: pages.account.tresory, scopes: ['president', 'treasurer'] }
+];
+
+export default function Tabs() {
+  const { pathname } = useRouter();
+  const activeLink = links.find((link) => link.href.search(pathname) !== -1);
+  let filteredLinks = [] as typeof links;
+
+  const { data: session } = useSession();
+  if (session) {
+    const userRoles = session.scopes ?? [];
+    filteredLinks = links.filter((link) => link.scopes.some((role) => userRoles.includes(role)));
+  }
+
+  return (
+    <div className='flex justify-center pr-2 md:border-r md:border-b-0 border-b md:mr-4 mb-4 md:mb-0'>
+      <div className='w-full max-w-md md:w-max flex justify-start items-center md:items-start md:flex-col flex-row mb-2 md:mb-0'>
+        {filteredLinks.map((link) => (
+          <Link
+            href={link.href}
+            key={link.href}
+            className={`px-3 py-2 text-sm w-full font-medium ${activeLink === link ? 'bg-slate-400 dark:bg-gray-800' : 'text-gray-900 hover:bg-slate-300 dark:text-gray-300 dark:hover:bg-gray-700'}`}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/account-page/account-page-user-form.tsx
@@ -0,0 +1,122 @@
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+
+import { useQueryClient } from '@tanstack/react-query';
+
+import { Button } from '@/components/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useReadAccountMe, useUpdateAccountMe } from '@/openapi-codegen/clochetteComponents';
+import { generateApiErrorMessage } from '@/openapi-codegen/clochetteFetcher';
+import { Account, OwnAccountUpdate } from '@/openapi-codegen/clochetteSchemas';
+
+interface UserFormProps {
+  user: Account;
+}
+
+export function UserForm(props: UserFormProps): JSX.Element {
+  const query = useQueryClient();
+
+  const { data: user } = useReadAccountMe(
+    {},
+    {
+      initialData: props.user
+    }
+  );
+
+  const updateOwnAccount = useUpdateAccountMe({
+    onSuccess: () => {
+      toast.success('Votre compte a été mis à jour.');
+      query.invalidateQueries({ stale: true });
+    },
+    onError: (error) => {
+      const detail = generateApiErrorMessage(error);
+      toast.error(`Une erreur est survenue lors de la mise à jour de votre compte : ${detail}`);
+    }
+  });
+
+  const form = useForm<OwnAccountUpdate>({
+    defaultValues: {
+      username: user?.username,
+      firstName: user?.firstName,
+      lastName: user?.lastName
+    }
+  });
+
+  const onSubmit = async (data: OwnAccountUpdate) => {
+    await updateOwnAccount.mutateAsync({
+      body: data
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className='grow flex justify-between flex-col gap-2'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name='firstName'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Prénom</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder='Prénom'
+                  {...field}
+                  value={field.value ?? ''}
+                />
+              </FormControl>
+              <FormDescription>Modification du prénom</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='lastName'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nom de famille</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder='Nom de famille'
+                  {...field}
+                  value={field.value ?? ''}
+                />
+              </FormControl>
+              <FormDescription>Modification du nom de famille</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nom d&apos;utilisateur</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Nom d'utilisateur"
+                  {...field}
+                  value={field.value ?? ''}
+                />
+              </FormControl>
+              <FormDescription>Modification du nom d&apos;utilisateur</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className='grow'></div>
+        <Button
+          type='submit'
+          loading={updateOwnAccount.isLoading}
+        >
+          Modifier mon compte
+        </Button>
+      </form>
+    </Form>
+  );
+}
