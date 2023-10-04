ba4eae405fb3770460e928b27700108ebc2c08e6
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-04 14:31:06 +0200

    🏷️ Update next auth and env types

---
+++ frontend/src/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import NextAuth, { DefaultSession, DefaultUser } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    scopes?: string[];
+    id: string;
+    token: string;
+  }
+  interface User {
+    sub: string;
+    iat: number;
+    exp: number;
+    token_type: string;
+    scopes?: string[];
+    id: string;
+    token: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT extends Record<string, unknown> {
+    sub: string;
+    iat: number;
+    exp: number;
+    token_type: string;
+    scopes?: string[];
+    token: string;
+  }
+}
