3153bf2082b8520d21bb837e0192190f31a8f18d
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-11 23:16:19 +0200

    🔨 Add default migrate in DeployScript for development

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -5,3 +5,7 @@ echo "Build the docker image"
 echo
 cd docker || exit
 docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+
+echo "Migrate DB"
+echo 
+docker exec -it clochette-backend poetry run python app/command.py migrate --bypass-revision
