3d0ab1911a222583be529fcd7855defdf8e154cc
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-11-20 10:40:31 +0100

    🔨 Add init and help option to DeployDocker script

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+# If first arg is help, then display help
+if [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: ./DeployDocker.sh [init]"
+    echo "  init: Init the DB, else migrate the DB"
+    exit 0
+fi
+
 # Build the docker image
 echo "Build the docker image"
 echo
 cd docker || exit
 docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
 
-echo "Migrate DB"
-echo 
-docker exec -it clochette-backend poetry run python app/command.py migrate --bypass-revision
+# If first arg is init, then init the DB
+if [ "$1" = "init" ]; then
+    echo "Init DB"
+    echo
+    docker exec -it clochette-backend poetry run python app/command.py init -y
+else
+    echo "Migrate DB"
+    echo
+    docker exec -it clochette-backend poetry run python app/command.py migrate --bypass-revision
+fi
